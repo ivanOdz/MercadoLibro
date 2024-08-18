f4d1792bf9fe0fab1791a2428b084f6e3194e92a
ivanOdz <miodzomek@gmail.com>
2024-08-18 20:06:22 -0300

    Jetty server initialization bug

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.paw.webapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@EnableWebMvc
+@ComponentScan({ "ar.edu.itba.paw.webapp.controller" })
+@Configuration
+public class WebConfig {
+
+
+}
