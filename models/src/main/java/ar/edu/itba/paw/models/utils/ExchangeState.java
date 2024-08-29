c117993844366ccb01273cc909c17d7d36c4a3b4
Maggie Taurian <mtaurian@itba.edu.ar>
2024-08-28 21:28:43 -0300

    added tables to data base

---
+++ models/src/main/java/ar/edu/itba/paw/models/utils/ExchangeState.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.paw.models.utils;
+
+public enum ExchangeState {
+    SUCCESFULL,
+    REJECTED,
+    PENDING
+}
---
+++ models/src/main/java/ar/edu/itba/paw/models/utils/PublicationState.java
@@ -0,0 +1,6 @@
+package ar.edu.itba.paw.models.utils;
+
+public enum PublicationState {
+    CURRENT,
+    TERMINATED
+}
