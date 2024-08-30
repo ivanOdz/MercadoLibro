906264a1734493a481b0d829ccf72d332e77db83
Maggie Taurian <mtaurian@itba.edu.ar>
2024-08-30 11:43:23 -0300

    Model edit: User.java

--- models/src/main/java/ar/edu/itba/paw/models/User.java
+++ models/src/main/java/ar/edu/itba/paw/models/User.java
@@ -2,19 +2,26 @@ package ar.edu.itba.paw.models;
 
 public class User {
 
-    private final long id;
+    private final long userId;
     private final String username;
+    private final String mail;
 
-    public User(long id, String username) {
-        this.id = id;
+    public User(long userId, String username, String mail) {
+        this.userId = userId;
         this.username = username;
+        this.mail = mail;
+    }
+
+    public long getId() {
+        return userId;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public long getId() {
-        return id;
+    public String getMail() {
+        return mail;
     }
+
 }
