148e1970b3e70dd16b82df41e63792e2305c5855
Maggie Taurian <mtaurian@itba.edu.ar>
2024-08-31 18:51:01 -0300

    Fixed bug because of Genres

--- models/src/main/java/ar/edu/itba/paw/models/utils/Genres.java
+++ models/src/main/java/ar/edu/itba/paw/models/utils/Genres.java
@@ -1,35 +1,44 @@
 package ar.edu.itba.paw.models.utils;
 
 public enum Genres {
-    FICTION,
-    NON_FICTION,
-    MYSTERY,
-    THRILLER,
-    SCIENCE_FICTION,
-    FANTASY,
-    ROMANCE,
-    HISTORICAL_FICTION,
-    HORROR,
-    BIOGRAPHY,
-    AUTOBIOGRAPHY,
-    MEMOIR,
-    YOUNG_ADULT,
-    CHILDRENS_LITERATURE,
-    GRAPHIC_NOVEL,
-    CLASSIC,
-    ADVENTURE,
-    DYSTOPIAN,
-    SELF_HELP,
-    POETRY,
-    LITERARY_FICTION,
-    CRIME,
-    WESTERN,
-    CONTEMPORARY,
-    RELIGIOUS_SPIRITUAL,
-    PHILOSOPHY,
-    SCIENCE,
-    TRAVEL,
-    TRUE_CRIME,
-    HISTORICAL_NON_FICTION;
-}
+    FICTION(1),
+    NON_FICTION(2),
+    MYSTERY(3),
+    THRILLER(4),
+    SCIENCE_FICTION(5),
+    FANTASY(6),
+    ROMANCE(7),
+    HISTORICAL_FICTION(8),
+    HORROR(9),
+    BIOGRAPHY(10),
+    AUTOBIOGRAPHY(11),
+    MEMOIR(12),
+    YOUNG_ADULT(13),
+    CHILDRENS_LITERATURE(14),
+    GRAPHIC_NOVEL(15),
+    CLASSIC(16),
+    ADVENTURE(17),
+    DYSTOPIAN(18),
+    SELF_HELP(19),
+    POETRY(20),
+    LITERARY_FICTION(21),
+    CRIME(22),
+    WESTERN(23),
+    CONTEMPORARY(24),
+    RELIGIOUS_SPIRITUAL(25),
+    PHILOSOPHY(26),
+    SCIENCE(27),
+    TRAVEL(28),
+    TRUE_CRIME(29),
+    HISTORICAL_NON_FICTION(30);
 
+    private final int value;
+
+    Genres(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
