92f0986f5c68148ccb4211e6d54cc016a143b01f
tscheffer1 <tscheffer@itba.edu.ar>
2024-09-02 23:22:10 -0300

    Genre bilingual and list mehtod

--- models/src/main/java/ar/edu/itba/paw/models/utils/Genres.java
+++ models/src/main/java/ar/edu/itba/paw/models/utils/Genres.java
@@ -1,54 +1,75 @@
 package ar.edu.itba.paw.models.utils;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Genres {
-    FICTION(1, "Fiction"),
-    NON_FICTION(2, "Non-Fiction"),
-    MYSTERY(3, "Mystery"),
-    THRILLER(4, "Thriller"),
-    SCIENCE_FICTION(5, "Science Fiction"),
-    FANTASY(6, "Fantasy"),
-    ROMANCE(7, "Romance"),
-    HISTORICAL_FICTION(8, "Historical Fiction"),
-    HORROR(9, "Horror"),
-    BIOGRAPHY(10, "Biography"),
-    AUTOBIOGRAPHY(11, "Autobiography"),
-    MEMOIR(12, "Memoir"),
-    YOUNG_ADULT(13, "Young Adult"),
-    CHILDRENS_LITERATURE(14, "Children's Literature"),
-    GRAPHIC_NOVEL(15, "Graphic Novel"),
-    CLASSIC(16, "Classic"),
-    ADVENTURE(17, "Adventure"),
-    DYSTOPIAN(18, "Dystopian"),
-    SELF_HELP(19, "Self-Help"),
-    POETRY(20, "Poetry"),
-    LITERARY_FICTION(21, "Literary Fiction"),
-    CRIME(22, "Crime"),
-    WESTERN(23, "Western"),
-    CONTEMPORARY(24, "Contemporary"),
-    RELIGIOUS_SPIRITUAL(25, "Religious/Spiritual"),
-    PHILOSOPHY(26, "Philosophy"),
-    SCIENCE(27, "Science"),
-    TRAVEL(28, "Travel"),
-    TRUE_CRIME(29, "True Crime"),
-    HISTORICAL_NON_FICTION(30, "Historical Non-Fiction");
-
-    private final int value;
-    private final String name;
-
-    Genres(int value, String name) {
-        this.value = value;
-        this.name = name;
+	
+	FICTION(1, "Fiction", "Ficción"),
+	NON_FICTION(2, "Non-Fiction", "No Ficción"),
+	MYSTERY(3, "Mystery", "Misterio"),
+	THRILLER(4, "Thriller", "Suspenso"),
+	SCIENCE_FICTION(5, "Science Fiction", "Ciencia Ficción"),
+	FANTASY(6, "Fantasy", "Fantasía"),
+	ROMANCE(7, "Romance", "Romance"),
+	HISTORICAL_FICTION(8, "Historical Fiction", "Ficción Histórica"),
+	HORROR(9, "Horror", "Horror"),
+	BIOGRAPHY(10, "Biography", "Biografía"),
+	AUTOBIOGRAPHY(11, "Autobiography", "Autobiografía"),
+	MEMOIR(12, "Memoir", "Memorias"),
+	YOUNG_ADULT(13, "Young Adult", "Juvenil"),
+	CHILDRENS_LITERATURE(14, "Children's Literature", "Literatura Infantil"),
+	GRAPHIC_NOVEL(15, "Graphic Novel", "Novela Gráfica"),
+	CLASSIC(16, "Classic", "Clásico"),
+	ADVENTURE(17, "Adventure", "Aventura"),
+	DYSTOPIAN(18, "Dystopian", "Distopía"),
+	SELF_HELP(19, "Self-Help", "Autoayuda"),
+	POETRY(20, "Poetry", "Poesía"),
+	LITERARY_FICTION(21, "Literary Fiction", "Ficción Literaria"),
+	CRIME(22, "Crime", "Crimen"),
+	WESTERN(23, "Western", "Oeste"),
+	CONTEMPORARY(24, "Contemporary", "Contemporáneo"),
+	RELIGIOUS_SPIRITUAL(25, "Religious/Spiritual", "Religioso/Espiritual"),
+	PHILOSOPHY(26, "Philosophy", "Filosofía"),
+	SCIENCE(27, "Science", "Ciencia"),
+	TRAVEL(28, "Travel", "Viajes"),
+	TRUE_CRIME(29, "True Crime", "Crimen Verdadero"),
+	HISTORICAL_NON_FICTION(30, "Historical Non-Fiction", "No Ficción Histórica");
+	
+	private final int value;
+	private final String englishName;
+	private final String spanishName;
+	
+	Genres(int value, String englishName, String spanishName) {
+		
+		this.value = value;
+		this.englishName = englishName;
+		this.spanishName = spanishName;
     }
-
-    public int getValue() {
-        return value;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public static Genres fromInt(int i) {
-        return Genres.values()[i];
-    }
+	
+	public String getName(Locale locale) {
+		
+		if (locale.getLanguage().equals("es")) {
+			return spanishName;
+		} else {
+			return englishName;
+		}
+	}
+    
+	public static List<String> getNames(Locale locale) {
+		
+		return Stream.of(Genres.values()).map(genre -> genre.getName(locale)).collect(Collectors.toList());
+	}
+	
+	public static Genres fromInt(int i) {
+		
+		return Genres.values()[i];
+	}
+	
+	public int getValue() {
+		
+		return value;
+	}
 }
