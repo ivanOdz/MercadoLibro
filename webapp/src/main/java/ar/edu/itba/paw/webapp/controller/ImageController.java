d135b3e46869017f67aaf0a4395859088ccb09a5
Maggie Taurian <mtaurian@itba.edu.ar>
2024-09-04 20:15:28 -0300

    Images working

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ImageController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ImageController.java
@@ -4,9 +4,7 @@ import ar.edu.itba.paw.interfaces.services.ImageService;
 import ar.edu.itba.paw.models.Image;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
