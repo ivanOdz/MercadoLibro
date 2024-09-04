5c81b0c8d257875bb2d2212fc36bbf8433223160
Maggie Taurian <mtaurian@itba.edu.ar>
2024-09-04 16:10:46 -0300

    Added getImageById method

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/persistence/ImageDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/persistence/ImageDao.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface ImageDao {
     Image createImage(byte[] image);
+    Optional<Image> getImageById(long imageId);
 }
--- persistence/src/main/java/ar/edu/itba/paw/persistence/ImageJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/ImageJdbcDao.java
@@ -1,9 +1,15 @@
 package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.interfaces.persistence.ImageDao;
+import ar.edu.itba.paw.models.Book;
 import ar.edu.itba.paw.models.Image;
 import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.models.utils.BookState;
+import ar.edu.itba.paw.models.utils.Genres;
+import ar.edu.itba.paw.models.utils.PublicationState;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +19,15 @@ import java.util.Optional;
 
 @Repository
 public class ImageJdbcDao implements ImageDao {
+
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert jdbcInsert;
+
+    private static final RowMapper<Image> ROWMAPPERIMAGE = (rs, rowNum) -> new Image(
+
+            rs.getLong("imageId"),
+            rs.getBytes("image")
+    );
 
     public ImageJdbcDao(final DataSource ds) {
         jdbcTemplate = new JdbcTemplate(ds);
@@ -29,4 +42,17 @@ public class ImageJdbcDao implements ImageDao {
         final Number generatedId = jdbcInsert.executeAndReturnKey(imageData);
         return new Image(generatedId.longValue(), image);
     }
+
+    @Override
+    public Optional<Image> getImageById(long imageId) {
+        String sql = "SELECT * FROM images WHERE imageId = ?";
+        try {
+            Image image = jdbcTemplate.queryForObject(sql, new Object[]{imageId}, ROWMAPPERIMAGE);
+            return Optional.ofNullable(image);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+
 }
