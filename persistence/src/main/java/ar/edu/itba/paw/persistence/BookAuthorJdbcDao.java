85a40d52e930ac09b3ecba91e877d2dbfc0bbf8b
jtechenski <jtechenski@itba.edu.ar>
2024-09-04 13:16:42 -0300

    merge error

--- persistence/src/main/java/ar/edu/itba/paw/persistence/BookAuthorJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/BookAuthorJdbcDao.java
@@ -4,12 +4,13 @@ import ar.edu.itba.paw.interfaces.persistence.BookAuthorDao;
 import ar.edu.itba.paw.models.BookAuthor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.util.Map;
 
-@Service
+@Repository
 public class BookAuthorJdbcDao implements BookAuthorDao {
 
     private final JdbcTemplate jdbcTemplate;
