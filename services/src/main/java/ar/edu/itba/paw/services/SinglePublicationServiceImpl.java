7a956f6cf6401c3fcfec3fff8e70a013abf88821
Maggie Taurian <mtaurian@itba.edu.ar>
2024-09-04 18:22:49 -0300

    Fixed service

--- services/src/main/java/ar/edu/itba/paw/services/SinglePublicationServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/SinglePublicationServiceImpl.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.persistence.*;
+import ar.edu.itba.paw.interfaces.services.*;
 import ar.edu.itba.paw.models.Author;
 import ar.edu.itba.paw.models.Book;
 import ar.edu.itba.paw.models.Publication;
@@ -8,7 +9,6 @@ import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.models.utils.BookState;
 import ar.edu.itba.paw.models.utils.Genres;
 import ar.edu.itba.paw.models.utils.PublicationState;
-import ar.edu.itba.paw.interfaces.services.SinglePublicationService;
 
 import org.springframework.stereotype.Service;
 
@@ -18,29 +18,29 @@ import java.util.List;
 public class SinglePublicationServiceImpl implements SinglePublicationService {
 
     private final SinglePublicationDao publicationDao;
-    private final UserDao userDao;
-    private final BookDao bookDao;
-    private final AuthorDao authorDao;
-    private final BookAuthorDao book_authorDao;
+    private final UserService userService;
+    private final BookService bookService;
+    private final AuthorService authorService;
+    private final BookAuthorService book_authorService;
 
-    public SinglePublicationServiceImpl(SinglePublicationDao publicationDao, UserDao userDao, BookDao bookDao, AuthorDao authorDao, BookAuthorDao bookAuthorDao) {
+    public SinglePublicationServiceImpl(SinglePublicationDao publicationDao, UserService userService, BookService bookService, AuthorService authorService, BookAuthorService book_authorService) {
         this.publicationDao = publicationDao;
-        this.userDao = userDao;
-        this.bookDao = bookDao;
-        this.authorDao = authorDao;
-        this.book_authorDao = bookAuthorDao;
+        this.userService = userService;
+        this.bookService = bookService;
+        this.authorService = authorService;
+        this.book_authorService = book_authorService;
     }
 
     @Override
     public Publication createPublication(String username, String mail, String isbn, String title, List<String> authors, String editorial, String description, Genres genre, BookState bookState, PublicationState publicationState, int edition, int rating, long image, String location) {
         
-    	User user = userDao.createUser(username, mail);
-        Book book = bookDao.createBook(isbn, title, editorial, description, genre, bookState, publicationState, edition, rating, image, user.getId());
+    	User user = userService.createUser(username, mail);
+        Book book = bookService.createBook(isbn, title, editorial, description, genre, bookState, publicationState, edition, rating, image, user.getId());
 
         for (String author : authors) {
             System.out.println("autor: " + author);
-            Author auth = authorDao.createAuthor(author);
-            book_authorDao.createBook_Author(book.getBookId(), auth.getAuthorId());
+            Author auth = authorService.createAuthor(author);
+            book_authorService.createBook_Author(book.getBookId(), auth.getAuthorId());
 
         }
         return publicationDao.createPublication(book.getBookId(), user.getId(), location);
