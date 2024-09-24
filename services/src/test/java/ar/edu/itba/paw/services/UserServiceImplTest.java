f8b60530738182df0353f6488a09d4e01b5e9a4f
tscheffer1 <tscheffer@itba.edu.ar>
2024-09-24 17:16:26 -0300

    UserService Test

--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -2,60 +2,73 @@ package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.interfaces.persistence.UserDao;
+import ar.edu.itba.paw.interfaces.services.EmailService;
+import ar.edu.itba.paw.interfaces.services.UserReviewService;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
-//@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
+	
+	private static final long USERID = 1;
+	private static final String USERNAME = "username";
+	private static final String MAIL = "user@mail.com";
+	private static final String PASSWORD = "password";
+	private static final Long IMAGEID = null;
+	private static final int VERIFICATIONCODE = 1234;
+	private static final boolean ISVERIFIED = true;
+	
+	@Mock
+	private UserDao userDao;
+	@Mock
+	private PasswordEncoder passwordEncoder;
+	@Mock
+	private EmailService emailService;
+	@Mock
+	private UserReviewService userReviewsService;
+	
+	@InjectMocks
+	private UserServiceImpl userService;
+	
+	@Test
+	public void testCreate() {
+		// 1. Precondiciones
+		String encodedPassword = "encodedPassword";
+		when (passwordEncoder.encode(PASSWORD)).thenReturn(encodedPassword);
+		
+		User mockUser = new User(USERID, USERNAME, MAIL, encodedPassword, IMAGEID, VERIFICATIONCODE, ISVERIFIED);
+		
+		when (userDao.createUser(eq(USERNAME), eq(MAIL), eq(encodedPassword), anyInt())).thenReturn(mockUser);
 
-    private static final String USERNAME = "username";
-    private static final String MAIL = "mail";
-    private static final String PASSWORD = "password";
-    private static final int VERIFICATIONCODE = 10;
-//----------------------------------------------
-    //@InjectMocks
-    private UserServiceImpl userService;
-    //@Mock
-    private UserDao mock;
-/* Este @Before es para cuando se corre con MockitoJUnitRunner => no se usa @Mock ni  @InjectMocks
-    @Before
-    public void setUp() {
-        mock = Mockito.mock(UserDao.class);
-        userService = new UserServiceImpl(mock);
-    }
-*/
-    /*@Test
-    public void testCreate() {
-        // 1. Precondiciones
-        Mockito.when(mock.createUser(eq(USERNAME), eq(MAIL), eq(PASSWORD), eq(VERIFICATIONCODE))).thenReturn(new User(1, USERNAME, MAIL, PASSWORD, 10, false));
-
-        // 2. Ejercito la class under test
-        User user = userService.createUser(USERNAME, MAIL, PASSWORD);
-
-        // 3. Valido las precondiciones
-        assertNotNull(user);
-        assertEquals(USERNAME, user.getUsername());
-        assertEquals(MAIL, user.getMail());
-        assertEquals(PASSWORD, user.getPassword());
-    }
-
-    @Test(expected = DuplicateKeyException.class)
-    public void testCreateDuplicate() {
-        // 1. Precondiciones
-        Mockito.when(mock.createUser(eq(USERNAME), eq(MAIL), eq(PASSWORD), eq(VERIFICATIONCODE))).thenThrow(DuplicateKeyException.class);
-
-        // 2. Ejercito la class under test
-        User user = userService.createUser(USERNAME, MAIL, PASSWORD);
-
-        // 3. Valido las precondiciones
-        fail();
-    }*/
+		// 2. Ejercita la clase bajo prueba
+		User user = userService.createUser(USERNAME, MAIL, PASSWORD);
+		
+		// 3. Valida las postcondiciones
+		assertNotNull(user);
+		assertEquals(USERNAME, user.getUsername());
+		assertEquals(MAIL, user.getMail());
+		assertEquals(encodedPassword, user.getPassword());
+	}
+	
+	@Test(expected = DuplicateKeyException.class)
+	public void testCreateDuplicate() {
+		
+		String encodedPassword = "encodedPassword";
+	    when (passwordEncoder.encode(PASSWORD)).thenReturn(encodedPassword);
+	    when (userDao.createUser(eq(USERNAME), eq(MAIL), eq(encodedPassword), anyInt()))
+	    	.thenThrow(new DuplicateKeyException("User already exists"));
+	
+	    userService.createUser(USERNAME, MAIL, PASSWORD);
+	}
 }
