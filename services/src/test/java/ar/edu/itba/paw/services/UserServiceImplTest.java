7d8edb05a42c4f8bdcacb7f1592627e7f8e263f3
tscheffer1 <tscheffer@itba.edu.ar>
2024-09-24 18:08:31 -0300

    PublicationDetailService Test

--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -21,13 +21,13 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
 	
-	private static final long USERID = 1;
-	private static final String USERNAME = "username";
+	private static final long USER_ID = 1;
+	private static final String USER_NAME = "userName";
 	private static final String MAIL = "user@mail.com";
 	private static final String PASSWORD = "password";
-	private static final Long IMAGEID = null;
-	private static final int VERIFICATIONCODE = 1234;
-	private static final boolean ISVERIFIED = true;
+	private static final Long IMAGE_ID = null;
+	private static final int VERIFICATION_CODE = 1234;
+	private static final boolean IS_VERIFIED = true;
 	
 	@Mock
 	private UserDao userDao;
@@ -45,18 +45,16 @@ public class UserServiceImplTest {
 	public void testCreate() {
 		// 1. Precondiciones
 		String encodedPassword = "encodedPassword";
+		User mockUser = new User(USER_ID, USER_NAME, MAIL, encodedPassword, IMAGE_ID, VERIFICATION_CODE, IS_VERIFIED);
 		when (passwordEncoder.encode(PASSWORD)).thenReturn(encodedPassword);
-		
-		User mockUser = new User(USERID, USERNAME, MAIL, encodedPassword, IMAGEID, VERIFICATIONCODE, ISVERIFIED);
-		
-		when (userDao.createUser(eq(USERNAME), eq(MAIL), eq(encodedPassword), anyInt())).thenReturn(mockUser);
+		when (userDao.createUser(eq(USER_NAME), eq(MAIL), eq(encodedPassword), anyInt())).thenReturn(mockUser);
 
 		// 2. Ejercita la clase bajo prueba
-		User user = userService.createUser(USERNAME, MAIL, PASSWORD);
+		User user = userService.createUser(USER_NAME, MAIL, PASSWORD);
 		
 		// 3. Valida las postcondiciones
 		assertNotNull(user);
-		assertEquals(USERNAME, user.getUsername());
+		assertEquals(USER_NAME, user.getUsername());
 		assertEquals(MAIL, user.getMail());
 		assertEquals(encodedPassword, user.getPassword());
 	}
@@ -66,9 +64,9 @@ public class UserServiceImplTest {
 		
 		String encodedPassword = "encodedPassword";
 	    when (passwordEncoder.encode(PASSWORD)).thenReturn(encodedPassword);
-	    when (userDao.createUser(eq(USERNAME), eq(MAIL), eq(encodedPassword), anyInt()))
-	    	.thenThrow(new DuplicateKeyException("User already exists"));
+	    when (userDao.createUser(eq(USER_NAME), eq(MAIL), eq(encodedPassword), anyInt()))
+	    	.thenThrow(new DuplicateKeyException("Usuario ya existe"));
 	
-	    userService.createUser(USERNAME, MAIL, PASSWORD);
+	    userService.createUser(USER_NAME, MAIL, PASSWORD);
 	}
 }
