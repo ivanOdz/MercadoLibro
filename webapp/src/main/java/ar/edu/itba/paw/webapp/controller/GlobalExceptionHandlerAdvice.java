dba1d3c92e6001361a0461fa3e2ace16afde2c31
ivanOdz <miodzomek@gmail.com>
2024-10-02 16:58:22 -0300

    GlobalExceptionHandlerAdvice implemented with ModelAndView

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GlobalExceptionHandlerAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GlobalExceptionHandlerAdvice.java
@@ -1,15 +1,16 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandlerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
-    public String handleRuntimeException(RuntimeException ex, Model model) {
-        model.addAttribute("errorMessage", "Ocurrió un error inesperado: " + ex.getMessage());
-        return "error/generic";
+    public ModelAndView handleRuntimeException(RuntimeException ex) {
+        ModelAndView mav = new ModelAndView("error/generic");
+        mav.addObject("errorMessage", "Ocurrió un error inesperado: " + ex.getMessage());
+        return mav;
     }
 }
