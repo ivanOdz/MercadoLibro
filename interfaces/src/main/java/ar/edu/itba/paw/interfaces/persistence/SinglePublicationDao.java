c6c8f47ed751adfed9d083e9a2165a56441284e3
ivanOdz <miodzomek@gmail.com>
2024-10-02 14:33:34 -0300

    Deleted unused services and daos, with their implementations

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/persistence/SinglePublicationDao.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.itba.paw.interfaces.persistence;
-
-import ar.edu.itba.paw.models.Publication;
-
-public interface SinglePublicationDao {
-     //Publication createPublication(long bookId, long userId, long locationId);
-
-}
--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/PublicationDetailService.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itba.paw.interfaces.services;
-
-import ar.edu.itba.paw.models.PublicationDetail;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface PublicationDetailService {
-    //PublicationDetail getPublicationDetail(long publicationId);
-}
