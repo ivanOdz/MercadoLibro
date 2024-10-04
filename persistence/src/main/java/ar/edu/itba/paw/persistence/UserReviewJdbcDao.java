11a0a2d1a3ac37040cb8447f96cf9ea38d607e03
tscheffer1 <tscheffer@itba.edu.ar>
2024-10-04 18:31:14 -0300

    Unknown column name problem resolved

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserReviewJdbcDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserReviewJdbcDao.java
@@ -32,13 +32,13 @@ public class UserReviewJdbcDao implements UserReviewDao {
 
 	private final JdbcTemplate jdbcTemplate;
     
-	String baseQuery = "SELECT userReview.userReviewId, userReview.reviewDescription, userReview.reviewDate, userReview.userReviewRating"
-						+ ", reviewer.userId AS reviewerUserId, reviewer.userName, reviewer.mail, reviewer.password, reviewer.imageId, reviewer.verificationCode, reviewer.isVerified, reviewer.language"
-						+ ", subject.userId AS subjectUserId, subject.userName, subject.mail, subject.password, subject.imageId, subject.verificationCode, subject.isVerified, subject.language"
-						+ ", exchange.exchangeId, exchange.exchangeState, exchange.acceptCode, exchange.offererReceivedBook, exchange.requesterReceivedBook"
-						+ ", publication.publicationId, publication.publicationState, publication.publicationDatetime"
-						+ ", bookModel.bookModelId, bookModel.isbn, bookModel.title, bookModel.editorial, bookModel.description, bookModel.genre, bookModel.edition, bookModel.weight, bookModel.pages, bookModel.bookLanguage, bookModel.dimension, bookModel.publicationYear, bookModel.isPocketEdition, bookModel.isHardCover, bookModel.imageId"
-						+ ", book.bookId, book.bookState, book.exchangesQty\r\n"
+	String baseQuery = "SELECT userReview.userReviewId AS userReviewId, userReview.reviewDescription AS userReviewDescription, userReview.reviewDate AS userReviewDate, userReview.userReviewRating AS userReviewRating\r\n"
+						+ ", reviewer.userId AS revieweruserid, reviewer.userName AS reviewerUserName, reviewer.mail AS reviewerMail, reviewer.password AS reviewerPassword, reviewer.imageId AS reviewerImageId, reviewer.verificationCode AS reviewerVerificationCode, reviewer.isVerified AS reviewerIsVerified, reviewer.language AS reviewerLanguage\r\n"
+						+ ", subject.userId AS subjectuserid, subject.userName AS subjectUserName, subject.mail AS subjectMail, subject.password AS subjectPassword, subject.imageId AS subjectImageId, subject.verificationCode AS subjectVerificationCode, subject.isVerified AS subjectIsVerified, subject.language AS subjectLanguage\r\n"
+						+ ", exchange.exchangeId AS exchangeId, exchange.offererPubId AS exchangeOffererPubId, exchange.exchangeState AS exchangeState, exchange.acceptCode AS exchangeAcceptCode, exchange.offererReceivedBook AS exchangeOffererReceivedBook, exchange.requesterReceivedBook AS exchangeRequesterReceivedBook\r\n"
+						+ ", publication.publicationId AS publicationId, publication.publicationState AS publicationState, publication.publicationDatetime AS publicationDateTime\r\n"
+						+ ", bookModel.bookModelId AS bookModelId, bookModel.isbn AS bookModelIsbn, bookModel.title AS bookModelTitle, bookModel.editorial AS bookModelEditorial, bookModel.description AS bookModelDescription, bookModel.genre AS bookModelGenre, bookModel.edition AS bookModelEdition, bookModel.weight AS bookModelWeight, bookModel.pages AS bookModelPages, bookModel.bookLanguage AS bookModelBookLanguage, bookModel.dimension AS bookModelDimension, bookModel.publicationYear AS bookModelPublicationYear, bookModel.isPocketEdition AS bookModelIsPocketEdition, bookModel.isHardCover AS bookModelIsHardCover, bookModel.imageId AS bookModelImageId\r\n"
+						+ ", book.bookId AS bookId, book.bookState AS bookState, book.exchangesQty AS bookExchangesQty\r\n"
 						+ " FROM user_review AS userReview\r\n"
 						+ " JOIN users AS reviewer ON userReview.reviewerId = reviewer.userId\r\n"
 						+ " JOIN users AS subject ON userReview.subjectId = subject.userId\r\n"
@@ -46,74 +46,74 @@ public class UserReviewJdbcDao implements UserReviewDao {
 						+ " JOIN publication AS publication ON userReview.subjectId = publication.userId AND (exchange.offererPubId = publication.publicationId OR exchange.requesterPubId = publication.publicationId)\r\n"
 						+ " JOIN book AS book ON publication.bookId = book.bookId\r\n"
 						+ " JOIN book_model AS bookModel ON book.bookModelId = bookModel.bookModelId\r\n"
-						+ " JOIN users AS actualOwner ON book.ownerId = actualOwner.userId\r\n";
+						+ " JOIN users AS actualOwner ON book.ownerId = actualOwner.userId";
     
 	private static final RowMapper<UserReview> ROW_MAPPER_USER_REVIEW =
 			
 		(rs, rowNum) -> {
 			
-			User reviewer = new User (	rs.getLong("reviewer.userId"),
-										rs.getString("reviewer.userName"),
-										rs.getString("reviewer.mail"),
-										rs.getString("reviewer.password"),
-										rs.getLong("reviewer.imageId"),
-										rs.getInt("reviewer.verificationCode"),
-										rs.getBoolean("reviewer.isVerified"),
-										rs.getString("reviewer.language")
+			User reviewer = new User (	rs.getLong("reviewerUserId"),
+										rs.getString("reviewerUserName"),
+										rs.getString("reviewerMail"),
+										rs.getString("reviewerPassword"),
+										rs.getLong("reviewerImageId"),
+										rs.getInt("reviewerVerificationCode"),
+										rs.getBoolean("reviewerIsVerified"),
+										rs.getString("reviewerLanguage")
 								 	);
 			
-			User subject = new User (	rs.getLong("subject.userId"),
-										rs.getString("subject.userName"),
-										rs.getString("subject.mail"),
-										rs.getString("subject.password"),
-										rs.getLong("subject.imageId"),
-										rs.getInt("subject.verificationCode"),
-										rs.getBoolean("subject.isVerified"),
-										rs.getString("subject.language")
+			User subject = new User (	rs.getLong("subjectUserId"),
+										rs.getString("subjectUserName"),
+										rs.getString("subjectMail"),
+										rs.getString("subjectPassword"),
+										rs.getLong("subjectImageId"),
+										rs.getInt("subjectVerificationCode"),
+										rs.getBoolean("subjectIsVerified"),
+										rs.getString("subjectLanguage")
 									);
 			
 			Rating rating = null;
 			String authors = null;
 			
-			BookModel bookModel = new BookModel(	rs.getLong("bookModel.bookModelId"),
-													rs.getString("bookModel.isbn"),
-													rs.getString("bookModel.title"),
-													rs.getString("bookModel.editorial"),
-													rs.getString("bookModel.description"),
-													Genre.fromInt(rs.getInt("bookModel.genre")),
-													rs.getInt("bookModel.edition"),
-													rs.getInt("bookModel.weight"),
-													rs.getInt("bookModel.pages"),
-													Language.fromInt(rs.getString("bookModel.bookLanguage") == "es-AR" ? Language.SPANISH.getValue() : Language.ENGLISH.getValue()),
-													rs.getInt("bookModel.dimension"),
-													rs.getShort("bookModel.publicationYear"),
-													rs.getBoolean("bookModel.isPocketEdition"),
-													rs.getBoolean("bookModel.isHardCover"),
+			BookModel bookModel = new BookModel(	rs.getLong("bookModelId"),
+													rs.getString("bookModelIsbn"),
+													rs.getString("bookModelTitle"),
+													rs.getString("bookModelEditorial"),
+													rs.getString("bookModelDescription"),
+													Genre.fromInt(rs.getInt("bookModelGenre")),
+													rs.getInt("bookModelEdition"),
+													rs.getInt("bookModelWeight"),
+													rs.getInt("bookModelPages"),
+													Language.fromInt(rs.getString("bookModelBookLanguage").equals("es-AR") ? Language.SPANISH.getValue() : Language.ENGLISH.getValue()),
+													rs.getInt("bookModelDimension"),
+													rs.getShort("bookModelPublicationYear"),
+													rs.getBoolean("bookModelIsPocketEdition"),
+													rs.getBoolean("bookModelIsHardCover"),
 													authors,
-													rs.getLong("bookModel.imageId"),
+													rs.getLong("bookModelImageId"),
 													rating
 												);
 			
-			Book bookEarned = new Book(	rs.getLong("book.bookId"),
+			Book bookEarned = new Book(	rs.getLong("bookId"),
 										reviewer,
 										bookModel,
-										BookState.fromInt(rs.getInt("book.bookState")),
-										rs.getInt("book.exchangesQty"),
+										BookState.fromInt(rs.getInt("bookState")),
+										rs.getInt("bookExchangesQty"),
 										false,	// isAvailable
 										null	// List<images>
 									);
 			
 			Location location = null;
 			
-			Publication publicationSelected = new Publication(	rs.getLong("publication.publicationId"),
-																bookEarned, PublicationState.fromInt(rs.getInt("publication.publicationState")),
-																rs.getTimestamp("publication.publicationDatetime"),
+			Publication publicationSelected = new Publication(	rs.getLong("publicationId"),
+																bookEarned, PublicationState.fromInt(rs.getInt("publicationState")),
+																rs.getTimestamp("publicationDatetime"),
 																location	
 															);	// Esta es la publicación que le interesó y por la cual intercambió el libro, o sea, contraria a la que hizo
 			Publication offererPublication = null;
 			Publication requesterPublication = null;
 			
-			if (rs.getLong("exchange.offererPubId") == rs.getLong("publication.publicationId")) {
+			if (rs.getLong("exchangeOffererPubId") == rs.getLong("publicationId")) {
 			
 				requesterPublication = publicationSelected;
 			}
@@ -122,22 +122,22 @@ public class UserReviewJdbcDao implements UserReviewDao {
 				offererPublication = publicationSelected;
 			}
 			
-			Exchange exchange = new Exchange(	rs.getLong("exchange.exchangeId"),
+			Exchange exchange = new Exchange(	rs.getLong("exchangeId"),
 												offererPublication,
 												requesterPublication,
-												ExchangeState.fromInt(rs.getInt("exchange.exchangeState")),
-												rs.getLong("exchange.acceptCode"),
-												rs.getBoolean("exchange.offererReceivedBook"),
-												rs.getBoolean("exchange.requesterReceivedBook")
+												ExchangeState.fromInt(rs.getInt("exchangeState")),
+												rs.getLong("exchangeAcceptCode"),
+												rs.getBoolean("exchangeOffererReceivedBook"),
+												rs.getBoolean("exchangeRequesterReceivedBook")
 											);
 			
-			UserReview userReview = new UserReview(	rs.getLong("userReview.userReviewId"),
+			UserReview userReview = new UserReview(	rs.getLong("userReviewId"),
 													reviewer,
 													subject,
 													exchange,
-													rs.getString("userReview.reviewDescription"),
-													rs.getTimestamp("userReview.reviewDate"),
-													rs.getInt("userReview.userReviewRating")
+													rs.getString("userReviewDescription"),
+													rs.getTimestamp("userReviewDate"),
+													rs.getInt("userReviewRating")
 												);
 			
 			return userReview;
@@ -153,7 +153,7 @@ public class UserReviewJdbcDao implements UserReviewDao {
 		
 		StringBuilder sqlQuery = new StringBuilder(baseQuery);
 		
-		sqlQuery.append(" WHERE reviewerUserId = ?");
+		sqlQuery.append(" WHERE reviewer.userId = ?");
 		
 		return jdbcTemplate.query(sqlQuery.toString(), new Object[]{ userId }, new int[]{ Types.BIGINT }, ROW_MAPPER_USER_REVIEW);
 	}
@@ -163,7 +163,7 @@ public class UserReviewJdbcDao implements UserReviewDao {
 		
 		StringBuilder sqlQuery = new StringBuilder(baseQuery);
 		
-		sqlQuery.append(" WHERE subjectUserId = ?");
+		sqlQuery.append(" WHERE subject.userId = ?");
 		
 		return jdbcTemplate.query(sqlQuery.toString(), new Object[]{ userId }, new int[]{ Types.BIGINT }, ROW_MAPPER_USER_REVIEW);
 	}
