fb3019929cf3e0f171a8e0c8a9acccddbe195b63
ivanOdz <miodzomek@gmail.com>
2024-10-05 18:37:32 -0300

    Pagination working in publication and book home

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PublicationController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PublicationController.java
@@ -49,7 +49,7 @@ public class PublicationController {
                               @RequestParam(name = "is-genre-filter-active", defaultValue = "false") boolean isGenreFilterActive,
                               @RequestParam(name = "genre-filter", required = false) Genre genreFilter,
                               @RequestParam(name = "sort-type", defaultValue = "PUBLICATION_DATE_ASCENDING") SortType sortType,
-                              @RequestParam(name = "currentPage", defaultValue = "0") int currentPage) {
+                              @RequestParam(name = "page", defaultValue = "0") int currentPage) {
 
         final ModelAndView mav = new ModelAndView("home/publications");
 
