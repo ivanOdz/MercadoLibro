70fd37b0793178e3bfece4c62701d3f57509ec4b
ivanOdz <miodzomek@gmail.com>
2024-10-05 10:19:55 -0300

    Pagination metadata clasess

---
+++ models/src/main/java/ar/edu/itba/paw/models/utils/pagination/BasicMetadata.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.paw.models.utils.pagination;
+
+public class BasicMetadata extends Metadata{
+    public BasicMetadata(int currentPage, int maxPage, int totalResults) {
+        super(currentPage, maxPage, totalResults);
+    }
+}
---
+++ models/src/main/java/ar/edu/itba/paw/models/utils/pagination/BookModelMetadata.java
@@ -0,0 +1,6 @@
+package ar.edu.itba.paw.models.utils.pagination;
+
+public class BookModelMetadata extends GenreFiltertadata {
+
+
+}
