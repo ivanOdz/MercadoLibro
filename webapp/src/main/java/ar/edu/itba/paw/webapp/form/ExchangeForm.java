83b01d99f9d768a0ba0060ba2bbe8118fa496d0f
Maggie Taurian <mtaurian@itba.edu.ar>
2024-10-06 00:41:11 -0300

    Added NotBlack to location in ExchangeForm

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/ExchangeForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/ExchangeForm.java
@@ -2,16 +2,23 @@ package ar.edu.itba.paw.webapp.form;
 
 import ar.edu.itba.paw.models.Book;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class ExchangeForm {
     long bookId;
+
+    @NotBlank
     String location;
+
     long publicationId;
 
     public long getBookId() {
         return bookId;
     }
 
-    public String getLocation() {
+    public @NotBlank String getLocation() {
         return location;
     }
 
@@ -23,7 +30,7 @@ public class ExchangeForm {
         this.bookId = bookId;
     }
 
-    public void setLocation(String location) {
+    public void setLocation(@NotBlank String location) {
         this.location = location;
     }
 
