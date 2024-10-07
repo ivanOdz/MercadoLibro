618161da1f9cd156b0caf96754a63e94a1db9453
tscheffer1 <tscheffer@itba.edu.ar>
2024-10-07 15:43:24 -0300

    User Test renovated, more Tests to be Refactored before uploading

--- persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/UserJdbcDaoTest.java
@@ -26,7 +26,7 @@ import static org.junit.Assert.*;
 @Rollback
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
-@Sql(scripts = "classpath:users.sql")
+@Sql(scripts = "classpath:users.sql")  // Carga los datos necesarios para las pruebas
 public class UserJdbcDaoTest {
     
     private static final String USERNAME = "username";
@@ -34,15 +34,15 @@ public class UserJdbcDaoTest {
     private static final String PASSWORD = "password";
     private static final String LANGUAGE = "en";
     private static final int VERIFICATIONCODE = 0;
-    
+
     @Autowired
     private UserJdbcDao userDao;
-    
+
     @Autowired
     private DataSource ds;
 
     private JdbcTemplate jdbcTemplate;
-    
+
     @Before
     public void setUp() {
         jdbcTemplate = new JdbcTemplate(ds);
@@ -52,11 +52,9 @@ public class UserJdbcDaoTest {
     public void testCreate() throws SQLException {
     	
         User user = userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
-        
         assertNotNull(user);
         assertEquals(USERNAME, user.getUsername());
         assertEquals(MAIL, user.getMail());
-
         assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "users", "username = '" + USERNAME + "'"));
     }
 
@@ -65,11 +63,73 @@ public class UserJdbcDaoTest {
     	
         User user = userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
         long userId = user.getUserId();
-        
         Optional<User> maybeUser = userDao.findById(userId);
-        
         assertTrue(maybeUser.isPresent());
         assertEquals(userId, maybeUser.get().getUserId());
     }
 
+    @Test
+    public void testFindByMail() throws SQLException {
+    	
+        userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
+        Optional<User> maybeUser = userDao.find(MAIL);
+        assertTrue(maybeUser.isPresent());
+        assertEquals(MAIL, maybeUser.get().getMail());
+    }
+
+    @Test
+    public void testFindByUsername() throws SQLException {
+    	
+        userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
+        Optional<User> maybeUser = userDao.findByUsername(USERNAME);
+        assertTrue(maybeUser.isPresent());
+        assertEquals(USERNAME, maybeUser.get().getUsername());
+    }
+
+    @Test
+    public void testUpdateUsername() throws SQLException {
+    	
+        User user = userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
+        long userId = user.getUserId();
+        String newUsername = "newUsername";
+        boolean updated = userDao.updateUsername(userId, newUsername);
+        assertTrue(updated);
+        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "users", "username = '" + newUsername + "'"));
+    }
+
+    @Test
+    public void testVerifyUser() throws SQLException {
+    	
+        userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
+        userDao.verifyUser(VERIFICATIONCODE);
+        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "users", "verificationCode IS NULL AND mail = '" + MAIL + "'"));
+    }
+
+    @Test
+    public void testChangePassword() throws SQLException {
+    	
+        userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
+        String newPassword = "newPassword";
+        userDao.changePassword(VERIFICATIONCODE, newPassword);
+        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "users", "password = '" + newPassword + "' AND mail = '" + MAIL + "'"));
+    }
+
+    @Test
+    public void testSetUserLanguage() throws SQLException {
+    	
+        User user = userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
+        long userId = user.getUserId();
+        String newLanguage = "es";
+        userDao.setUserLanguage(userId, newLanguage);
+        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "users", "language = '" + newLanguage + "' AND userId = " + userId));
+    }
+
+    @Test
+    public void testGetUserToVerify() throws SQLException {
+    	
+        userDao.createUser(USERNAME, MAIL, PASSWORD, LANGUAGE, VERIFICATIONCODE);
+        User user = userDao.getUserToVerify(VERIFICATIONCODE);
+        assertNotNull(user);
+        assertEquals(MAIL, user.getMail());
+    }
 }
