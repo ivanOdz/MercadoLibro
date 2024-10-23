4907fd30a29aea4b66290fadb3515c3c71ded937
tscheffer1 <tscheffer@itba.edu.ar>
2024-10-23 20:13:50 -0300

    Working on adapting imageJdbc to jpa

--- models/src/main/java/ar/edu/itba/paw/models/Image.java
+++ models/src/main/java/ar/edu/itba/paw/models/Image.java
@@ -1,19 +1,45 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "image")
 public class Image {
-    private final Integer imageId;
-    private final byte[] image;
 
-    public Image(Integer imageId, byte[] image) {
-        this.imageId = imageId;
-        this.image = image;
-    }
-
-    public Integer getImageId() {
-        return imageId;
-    }
-
-    public byte[] getImage() {
-        return image;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer imageId;
+	
+	@Lob
+	@Column(name = "image", nullable = false)
+	private byte[] image;
+	
+	public Image() {}
+	
+	public Image(Integer imageId, byte[] image) {
+		this.imageId = imageId;
+		this.image = image;
+	}
+	
+	public Integer getImageId() {
+		return imageId;
+	}
+	
+	public void setImageId(Integer imageId) {
+		this.imageId = imageId;
+	}
+	
+	public byte[] getImage() {
+		return image;
+	}
+	
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
 }
---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/ImageJpaDao.java
@@ -0,0 +1,49 @@
+package ar.edu.itba.paw.persistence;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.itba.paw.interfaces.persistence.ImageDao;
+import ar.edu.itba.paw.models.Image;
+
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class ImageJpaDao implements ImageDao {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	@Transactional
+	public Image createImage(byte[] image) {
+		
+		Image img = new Image();
+		img.setImage(image);
+		entityManager.persist(img);
+		return img;
+	}
+	
+	@Override
+	public Optional<Image> getImageById(long imageId) {
+	
+	Image image = entityManager.find(Image.class, imageId);
+		return Optional.ofNullable(image);
+	}
+	
+	@Override
+	public Image getFirstImageByBookId(long bookId) {
+		
+		String query = "SELECT i FROM Image i JOIN BookImage b ON i.imageId = b.image.imageId " +
+		   "WHERE b.book.bookId = :bookId AND b.imageOrder = 0";
+		
+		return entityManager.createQuery(query, Image.class)
+		.setParameter("bookId", bookId)
+		.getResultList()
+		.stream()
+		.findFirst()
+		.orElse(null);
+	}
+}
