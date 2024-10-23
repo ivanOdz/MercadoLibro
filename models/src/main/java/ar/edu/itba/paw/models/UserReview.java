89f331b30aedb3eff0e55e9cb1797c55abfd9ef6
Maggie Taurian <mtaurian@itba.edu.ar>
2024-10-23 20:57:46 -0300

    Adapted UserReview model for hibernate

--- models/src/main/java/ar/edu/itba/paw/models/UserReview.java
+++ models/src/main/java/ar/edu/itba/paw/models/UserReview.java
@@ -1,17 +1,43 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
+@Entity
+@Table(name = "user_review")
 public class UserReview {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_review_userreviewid_seq")
+	@SequenceGenerator(sequenceName = "user_review_userreviewid_seq", name = "user_review_userreviewid_seq", allocationSize = 1)
+	@Column(name = "userReviewId")
 	private long userReviewId;
+
+	@ManyToOne
+	@JoinColumn(name = "reviewerId", referencedColumnName = "userId")
 	private User reviewer;
+
+	@ManyToOne
+	@JoinColumn(name = "subjectId", referencedColumnName = "userId")
 	private User subject;
+
+	@ManyToOne
+	@JoinColumn(name = "exchangeId", referencedColumnName = "exchangeId")
 	private Exchange exchange;
+
+	@Column(name = "reviewDescription")
 	private String reviewDescription;
+
+	@Column(name = "reviewDate")
 	private Timestamp reviewDate;
+
+	@Column(name = "reviewRating")
 	private int reviewRating;
-	
+
+	public UserReview() {
+		// Only for JPA
+	}
+
 	public UserReview(long userReviewId, User reviewer, User subject, Exchange exchange, String reviewDescription, Timestamp reviewDate, int reviewRating) {
 		
 		this.userReviewId = userReviewId;
