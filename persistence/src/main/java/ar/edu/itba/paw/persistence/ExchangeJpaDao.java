26d703ac208918929c0ec8709aba049c0eef624e
Maggie Taurian <mtaurian@itba.edu.ar>
2024-10-23 20:07:24 -0300

    Updated getAllExchangesByUserId function in ExchangeJpaDao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/ExchangeJpaDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/ExchangeJpaDao.java
@@ -14,7 +14,13 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.sql.Timestamp;
+import java.util.List;
+
+import static ar.edu.itba.paw.models.utils.Constants.BOOKS_PAGE_SIZE;
+import static ar.edu.itba.paw.models.utils.Constants.EXCHANGES_PAGE_SIZE;
 
 @Primary
 @Repository
@@ -92,6 +98,27 @@ public class ExchangeJpaDao implements ExchangeDao {
 
     @Override
     public PaginatedResponse<Exchange, BasicMetadata> getAllExchangesByUserId(long anUserId, ExchangeState exchangeState, int currentPage, boolean isOfferer) {
-        return null;
+        if(currentPage < 0){
+            currentPage = 0;
+        }
+
+        StringBuilder queryString = new StringBuilder("SELECT e.exchangeId FROM Exchange e WHERE ");
+
+        if (isOfferer) {
+            queryString.append("e.offerer.userId = :userId");
+        } else {
+            queryString.append("e.requester.userId = :userId");
+        }
+
+        Query nativeQuery = em.createQuery(queryString.toString());
+        nativeQuery.setMaxResults(EXCHANGES_PAGE_SIZE);
+        nativeQuery.setFirstResult(currentPage * EXCHANGES_PAGE_SIZE);
+
+        List<Long> exchangeIds = nativeQuery.getResultList();
+
+        TypedQuery<Exchange> query = em.createQuery("FROM Exchange e WHERE e.exchangeId IN (:ids) AND e.state = :state", Exchange.class);
+        query.setParameter("ids", exchangeIds);
+
+        return new PaginatedResponse<>(query.getResultList(), new BasicMetadata(currentPage, EXCHANGES_PAGE_SIZE, exchangeIds.size()));
     }
 }
