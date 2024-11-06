393fe271f0e60a861f0f1a0356bde770e117a83f
modzomek <modzomek@itba.edu.ar>
2024-11-06 16:15:56 -0300

    Added response status codes to error controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -4,42 +4,36 @@ import ar.edu.itba.paw.interfaces.exceptions.base.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ErrorController {
 
-//    @Autowired
-//    private final MessageSource messageSource;
-//
-//    public ErrorController(MessageSource messageSource) {
-//        this.messageSource = messageSource;
-//    }
-//
     @RequestMapping("/400")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ModelAndView badRequest() {
         return new ModelAndView("error/400");
     }
 
     @RequestMapping("/403")
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     public ModelAndView forbidden() {
         return new ModelAndView("error/403");
     }
 
     @RequestMapping("/404")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ModelAndView notFound() {
         return new ModelAndView("error/404");
     }
 
     @RequestMapping("/500")
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView internalServerError() {
         return new ModelAndView("error/500");
     }
-//
-//    @RequestMapping("/**")
-//    public ModelAndView handleAll() {
-//        return new ModelAndView("error/notFound");
-//    }
 }
