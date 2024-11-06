9dd58e2fa5b954620f8c854e68ffddf762146e30
tscheffer1 <tscheffer@itba.edu.ar>
2024-11-05 21:07:22 -0300

    Silly me, again forgot to upload new Files

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/constants/BookConstants.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.paw.persistence.constants;
+
+
+public final class BookConstants {
+	// (bookId, bookModelId, ownerId, exchangesQty, available, bookState)
+	
+	public static final long ID_1 = 1;
+	public static final long BOOK_MODEL_ID_1 = 1;
+	public static final long OWNER_ID_1 = 1;
+	public static final Integer EXCHANGE_QTY_1 = 1;
+	public static final Boolean AVAILABLE_1 = true;
+	public static final String BOOK_STATE_1 = "WORN";
+	
+}
