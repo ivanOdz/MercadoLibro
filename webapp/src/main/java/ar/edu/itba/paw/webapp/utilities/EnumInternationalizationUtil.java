600b0d8658c518414a5c073ef9c1d4e99e831a73
ivanOdz <miodzomek@gmail.com>
2024-11-07 18:16:28 -0300

    Changing class to final class as it cannot be instantiated

--- webapp/src/main/java/ar/edu/itba/paw/webapp/utilities/EnumInternationalizationUtil.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/utilities/EnumInternationalizationUtil.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class EnumInternationalizationUtil {
+public final class EnumInternationalizationUtil {
 
     private EnumInternationalizationUtil() {
         // To prevent instantiation
