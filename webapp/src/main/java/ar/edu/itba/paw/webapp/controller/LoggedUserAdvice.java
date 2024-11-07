13a409eb991268710298ed55edb48a744516da50
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-11-07 19:32:14 -0300

    To maintain coherency of spring injection, using @Autowired instead of constructor.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/LoggedUserAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/LoggedUserAdvice.java
@@ -5,6 +5,7 @@ import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.webapp.auth.PawUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,11 +17,9 @@ import java.util.Optional;
 public class LoggedUserAdvice {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggedUserAdvice.class);
-    final UserService us;
 
-    public LoggedUserAdvice(UserService userService) {
-        this.us = userService;
-    }
+    @Autowired
+    private UserService us;
 
     // Binding false because it is a read-onlu attribute
     @ModelAttribute(name = "loggedUser", binding = false)
