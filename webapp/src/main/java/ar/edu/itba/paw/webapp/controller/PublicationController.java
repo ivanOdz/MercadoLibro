959f85d567a0bdc2673ee4d6fcb652b2c30a61d1
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-11-08 20:43:16 -0300

    try catch blocks in PublicationController and User controller removed

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PublicationController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PublicationController.java
@@ -10,8 +10,6 @@ import ar.edu.itba.paw.webapp.form.ExchangeForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
