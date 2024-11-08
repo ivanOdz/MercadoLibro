60b9b6fba23593d987d4b14233ae57f14736e97f
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-11-08 12:57:37 -0300

    deleting exceptions

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/exceptions/BookAuthorBadRequestException.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itba.paw.interfaces.exceptions;
-
-import ar.edu.itba.paw.interfaces.exceptions.base.BadRequestException;
-
-public class BookAuthorBadRequestException extends BadRequestException {
-    public BookAuthorBadRequestException(String message) {
-        super(message);
-    }
-}
