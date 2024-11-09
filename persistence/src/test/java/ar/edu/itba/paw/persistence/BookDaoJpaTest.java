a1890089ba50a2b90831d9704a3c7e8b5a20e1c1
tscheffer1 <tscheffer@itba.edu.ar>
2024-11-09 00:19:26 -0300

    Book Test with extrange error: A collection with cascade="all-delete-orphan" was no longer referenced by the owning entity instance: ar.edu.itba.paw.models.Book.images

--- persistence/src/test/java/ar/edu/itba/paw/persistence/BookDaoJpaTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/BookDaoJpaTest.java
@@ -14,13 +14,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.itba.paw.interfaces.persistence.BookDao;
 import ar.edu.itba.paw.models.Author;
 import ar.edu.itba.paw.models.Book;
+import ar.edu.itba.paw.models.BookImage;
 import ar.edu.itba.paw.models.BookModel;
 import ar.edu.itba.paw.models.Image;
 import ar.edu.itba.paw.models.User;
@@ -55,9 +58,6 @@ public class BookDaoJpaTest {
 		
 		jdbcTemplate = new JdbcTemplate(ds);
 	}
-	// void createBookRating(User user, BookModel bookModel, int rating);
-	// void createBookImage(Book book, List<Image> images);
-	// void setOwner(Book book, User user);
 	
 	@Test
 	public void testGetBookById() {
@@ -73,6 +73,7 @@ public class BookDaoJpaTest {
 	}
 	
 	@Test
+	@Rollback
 	public void testCreateBook() {
 		
 		final Author author = em.merge(new Author(AuthorConstants.ID_8, AuthorConstants.NAME_8));
@@ -109,4 +110,76 @@ public class BookDaoJpaTest {
 		Assert.assertEquals(bookModel, newBook.getBookModel());
 		Assert.assertEquals(user, newBook.getOwner());
 	}
+	
+	@Test
+	@Rollback
+	public void testCreateBookRating() {
+		
+		final Author author = em.merge(new Author(AuthorConstants.ID_2, AuthorConstants.NAME_2));
+		final List<Author> authors = new ArrayList<Author>();
+		authors.add(author);
+		
+		final BookModel bookModel = em.merge(new BookModel(	BookModelConstants.ID_2,
+															BookModelConstants.ISBN_2,
+															BookModelConstants.TITLE_2,
+															BookModelConstants.EDITORIAL_2,
+															BookModelConstants.DESCRIPTION_2,
+															Genre.fromString(BookModelConstants.GENRE_2),
+															(int)BookModelConstants.EDITION_2,
+															(int)BookModelConstants.WEIGHT_2,
+															(int)BookModelConstants.PAGES_2,
+															Language.valueOf(BookModelConstants.LANGUAGE_2),
+															BookDimension.valueOf(BookModelConstants.DIMENSION_2),
+															(short)(int)BookModelConstants.PUBLICATION_YEAR_2,
+															BookModelConstants.IS_POCKET_EDITION_2,
+															BookModelConstants.IS_HARD_COVER_2,
+															authors,
+															null
+														));
+		
+		final User user = em.merge(new User(UserConstants.ID_1, UserConstants.NAME_1, UserConstants.MAIL_1, UserConstants.PASSWORD_1, UserConstants.IMAGE_ID_1, UserConstants.VERIFICATION_CODE_1, UserConstants.IS_VERIFIED_1, UserConstants.LANGUAGE_1));
+		
+		bookDao.createBookRating(user, bookModel, 4);
+		em.flush();
+		
+		Assert.assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "book_rating", "userId = " + user.getUserId() + " AND bookModelId = '" + bookModel.getBookModelId() + "' AND rating = 4"));
+	}
+	
+	// ERROR EXTRAÑO!
+	
+//	@Test
+//	@Rollback
+//	public void testSetOwner() {
+//		
+//		final Author author = em.merge(new Author(AuthorConstants.ID_2, AuthorConstants.NAME_2));
+//		final List<Author> authors = new ArrayList<Author>();
+//		authors.add(author);
+//		
+//		final BookModel bookModel = em.merge(new BookModel(	BookModelConstants.ID_2,
+//															BookModelConstants.ISBN_2,
+//															BookModelConstants.TITLE_2,
+//															BookModelConstants.EDITORIAL_2,
+//															BookModelConstants.DESCRIPTION_2,
+//															Genre.fromString(BookModelConstants.GENRE_2),
+//															(int)BookModelConstants.EDITION_2,
+//															(int)BookModelConstants.WEIGHT_2,
+//															(int)BookModelConstants.PAGES_2,
+//															Language.valueOf(BookModelConstants.LANGUAGE_2),
+//															BookDimension.valueOf(BookModelConstants.DIMENSION_2),
+//															(short)(int)BookModelConstants.PUBLICATION_YEAR_2,
+//															BookModelConstants.IS_POCKET_EDITION_2,
+//															BookModelConstants.IS_HARD_COVER_2,
+//															authors,
+//															null
+//														));
+//		
+//		final User oldOwner = em.merge(new User(UserConstants.ID_1, UserConstants.NAME_1, UserConstants.MAIL_1, UserConstants.PASSWORD_1, UserConstants.IMAGE_ID_1, UserConstants.VERIFICATION_CODE_1, UserConstants.IS_VERIFIED_1, UserConstants.LANGUAGE_1));
+//		final User newOwner = em.merge(new User(UserConstants.ID_2, UserConstants.NAME_2, UserConstants.MAIL_2, UserConstants.PASSWORD_2, UserConstants.IMAGE_ID_2, UserConstants.VERIFICATION_CODE_2, UserConstants.IS_VERIFIED_2, UserConstants.LANGUAGE_2));
+//		final Book book = em.merge(new Book(BookConstants.ID_2, oldOwner, bookModel, BookState.fromString(BookConstants.BOOK_STATE_2), (int)BookConstants.EXCHANGE_QTY_2, BookConstants.AVAILABLE_2, null));
+//		
+//		bookDao.setOwner(book, newOwner);
+//		em.flush();
+//		
+//		Assert.assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "book", "ownerId = " + newOwner.getUserId() + " AND bookModelId = " + bookModel.getBookModelId()));
+//	}
 }
