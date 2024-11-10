71138a736d8e842770370f74b39565b9831b1d08
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-11-10 17:00:49 -0300

    Loggin in LocationServiceImpl

--- services/src/main/java/ar/edu/itba/paw/services/LocationServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/LocationServiceImpl.java
@@ -4,12 +4,13 @@ import ar.edu.itba.paw.interfaces.exceptions.LocationNotFoundException;
 import ar.edu.itba.paw.interfaces.persistence.LocationDao;
 import ar.edu.itba.paw.interfaces.services.LocationService;
 import ar.edu.itba.paw.models.Location;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class LocationServiceImpl implements LocationService {
@@ -17,26 +18,37 @@ public class LocationServiceImpl implements LocationService {
     @Autowired
     private LocationDao locationDao;
 
-
-    @Override
-    @Transactional(readOnly = true)
-    public Set<Location> getLocationsByPublicationId(long pubId) {
-        return locationDao.getLocationByPublicationId(pubId);
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocationServiceImpl.class);
 
     @Override
     @Transactional
     public Location newLocation(String location) {
-        return locationDao.newLocation(location);
+        LOGGER.info("Creating new location: {}", location);
+
+        Location newLocation = locationDao.newLocation(location);
+
+        if (newLocation == null) {
+            LOGGER.warn("Failed to create new location: {}", location);
+        } else {
+            LOGGER.info("New location created successfully: {}", location);
+        }
+
+        return newLocation;
     }
 
     @Override
     @Transactional(readOnly = true)
     public Location findById(Long locationId) {
+        LOGGER.info("Searching for location with ID: {}", locationId);
+
         Optional<Location> location = locationDao.findById(locationId);
-        if(location.isEmpty()){
+
+        if (location.isEmpty()) {
+            LOGGER.warn("Location with ID: {} not found", locationId);
             throw new LocationNotFoundException("Location not found");
         }
+
+        LOGGER.info("Location with ID: {} found", locationId);
         return location.get();
     }
 }
