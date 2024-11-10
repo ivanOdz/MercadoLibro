386da8a390fb96c93359e7ac17ee52167212cf76
tscheffer1 <tscheffer@itba.edu.ar>
2024-11-09 23:51:50 -0300

    Get test in user_review

--- persistence/src/test/java/ar/edu/itba/paw/persistence/UserReviewDaoJpaTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/UserReviewDaoJpaTest.java
@@ -1,13 +1,12 @@
 package ar.edu.itba.paw.persistence;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -40,6 +39,8 @@ public class UserReviewDaoJpaTest {
     
 	private JdbcTemplate jdbcTemplate;
 	
+	private final Long offererUserId = 1L;
+	private final Long requesterUserId = 1L;
 	private final Long exchangeId = 1L;
 	private final Long offererPubId = 2L;
 	private final Long requesterPubId = 4L;
@@ -60,14 +61,6 @@ public class UserReviewDaoJpaTest {
 		jdbcTemplate.update(" 	UPDATE book SET ownerId = " + BookConstants.OWNER_ID_2 + "WHERE bookId = " + BookConstants.ID_1);
 	}
 	
-//	PaginatedResponse<UserReview, BasicMetadata> getReviewsGivenByUserId(long userId, int currentPage);
-//	PaginatedResponse<UserReview, BasicMetadata> getReviewsEarnedByUserId(long userId, int currentPage);
-//	Optional<UserReview> getUserReviewEarned(long exchangeId, long userId);
-//	Optional<UserReview> getUserReviewGiven(long exchangeId, long userId);
-//	Optional<Rating> getUserRatingEarned(long userId);
-//	Optional<Rating> getUserRatingGiven(long userId);
-	
-//	UserReview createOrUpdateUserReview(long exchangeId, long userId, long userSubjectId, String description, int rating);
 	@Test
 	@Rollback
 	public void testCreateOrUpdateUserReview() {
@@ -75,10 +68,12 @@ public class UserReviewDaoJpaTest {
 		final String description = "OK!";
 		final int rating = 3;
 		
-		UserReview newReview = userReviewDao.createOrUpdateUserReview(exchangeId, (long)offererPubId, (long)requesterPubId, description, rating);
+		UserReview newReview = userReviewDao.createOrUpdateUserReview(exchangeId, (long)offererUserId, (long)requesterUserId, description, rating);
 		
-		assertNotNull(newReview);
-		
-		
+		Assert.assertNotNull(newReview);
+		Assert.assertEquals(exchangeId, newReview.getExchange().getExchangeId());
+		Assert.assertEquals(offererUserId, newReview.getUserReviewId());
+		Assert.assertEquals(rating, newReview.getReviewRating());
+		Assert.assertEquals(description, newReview.getReviewDescription());
 	}
 }
