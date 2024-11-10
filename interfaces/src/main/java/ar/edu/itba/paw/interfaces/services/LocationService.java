f9efbd2442e444a1e7fd49bb97c59acc408f3249
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-11-10 17:01:15 -0300

    Deleted unused method in LocationService

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/LocationService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/LocationService.java
@@ -2,11 +2,7 @@ package ar.edu.itba.paw.interfaces.services;
 
 import ar.edu.itba.paw.models.Location;
 
-import java.util.Set;
-
 public interface LocationService {
-    Set<Location> getLocationsByPublicationId(long pubId);
-
     Location newLocation(String location);
 
     Location findById(Long locationId);
