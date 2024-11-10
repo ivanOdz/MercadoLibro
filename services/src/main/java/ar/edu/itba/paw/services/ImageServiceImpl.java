fa30da9a04c74747edfb28bbf068f07b48207069
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-11-10 17:14:00 -0300

    Logging in ImageServiceImpl

--- services/src/main/java/ar/edu/itba/paw/services/ImageServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/ImageServiceImpl.java
@@ -5,6 +5,8 @@ import ar.edu.itba.paw.interfaces.exceptions.ImageNotFoundException;
 import ar.edu.itba.paw.interfaces.persistence.ImageDao;
 import ar.edu.itba.paw.interfaces.services.ImageService;
 import ar.edu.itba.paw.models.Image;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,29 +23,41 @@ public class ImageServiceImpl implements ImageService {
     @Autowired
     private ImageDao imageDao;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageServiceImpl.class);
 
     @Override
     @Transactional
     public List<Image> saveImage(List<MultipartFile> image) {
+        LOGGER.info("Starting to save images, total images: {}", image.size());
+
         List<Image> images = new ArrayList<>();
         for(MultipartFile file : image) {
             try {
+                LOGGER.info("Saving image with original filename: {}", file.getOriginalFilename());
                 images.add(imageDao.createImage(file.getBytes()));
+                LOGGER.info("Image with filename {} saved successfully", file.getOriginalFilename());
             } catch (IOException e) {
+                LOGGER.warn("Error saving image with filename: {}", file.getOriginalFilename());
                 throw new ImageBadRequestException("Error saving image");
             }
         }
 
+        LOGGER.info("Finished saving images, total saved: {}", images.size());
         return images;
     }
 
     @Override
     @Transactional(readOnly = true)
     public Image getImageById(Long imageId) {
+        LOGGER.info("Fetching image with ID: {}", imageId);
+
         Optional<Image> image = imageDao.getImageById(imageId);
         if (image.isEmpty()){
+            LOGGER.warn("Image with ID {} not found", imageId);
             throw new ImageNotFoundException("Image not found");
         }
+
+        LOGGER.info("Image with ID {} found", imageId);
         return image.get();
     }
 }
