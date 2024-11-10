72821878a536f54af83b8edf2cef7050bc133c2f
tscheffer1 <tscheffer@itba.edu.ar>
2024-11-10 18:53:38 -0300

    Publication Service Tests

--- services/src/test/java/ar/edu/itba/paw/services/PublicationServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/PublicationServiceImplTest.java
@@ -8,83 +8,103 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import ar.edu.itba.paw.interfaces.services.BookModelService;
+import ar.edu.itba.paw.interfaces.exceptions.PublicationNotFoundException;
+import ar.edu.itba.paw.interfaces.exceptions.UserNotUnauthorizedException;
+import ar.edu.itba.paw.interfaces.persistence.PublicationDao;
 import ar.edu.itba.paw.interfaces.services.BookService;
+import ar.edu.itba.paw.interfaces.services.LocationService;
 import ar.edu.itba.paw.interfaces.services.PublicationService;
+import ar.edu.itba.paw.interfaces.services.UserService;
 import ar.edu.itba.paw.models.Book;
-import ar.edu.itba.paw.models.BookImage;
-import ar.edu.itba.paw.models.BookModel;
+import ar.edu.itba.paw.models.Location;
 import ar.edu.itba.paw.models.Publication;
 import ar.edu.itba.paw.models.User;
-import ar.edu.itba.paw.models.utils.BookState;
-import ar.edu.itba.paw.models.utils.Genre;
-import ar.edu.itba.paw.models.utils.Language;
 import ar.edu.itba.paw.models.utils.PublicationState;
-import ar.edu.itba.paw.models.utils.Rating;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class PublicationServiceImplTest {
-//
-//    @Mock
-//    private PublicationService publicationService;
-//    @Mock
-//    private BookService bookService;
-//    @Mock
-//    private BookModelService bookModelService;
-//
-//    @InjectMocks
-//    private PublicationServiceImpl publicationDetailService;
-//
-//    private static final long PUBLICATION_ID = 1;
-//    private static final long BOOK_ID = 100;
-//    private static final long BOOK_MODEL_ID = 200;
-//    private static final long OWNER_ID = 300;
-//    private static final int EXCHANGES_QTY = 5;
-//    private static final int RATING = 4;
-//    private static final long IMAGE_ID = 400;
-//    private static final int IMAGE_ORDER = 1;
-//    private static final Timestamp IMAGE_DATETIME = new Timestamp(System.currentTimeMillis());
-//    
-//    @Test
-//    public void testGetPublicationDetail() {
-//        
-//        Book mockBook = new Book(BOOK_ID, new User(OWNER_ID, "username", "mail", "password", IMAGE_ID, 1234, true, "EN"), 
-//                                 new BookModel(BOOK_MODEL_ID, "9874567890123", "Titulo de libro", "Nombre de editorial", 
-//                                 "Esta es mi descripcion", Genre.FICTION, 1, 500, 300, Language.SPANISH, 
-//                                 20, (short) 2022, false, true, "Autor 1, Autor 2", IMAGE_ID, null), 
-//                                 BookState.NEW, EXCHANGES_QTY, true, new ArrayList<>()
-//                                 );
-//        
-//        Publication mockPublication = new Publication(PUBLICATION_ID, mockBook, PublicationState.CURRENT, IMAGE_DATETIME, null);
-//        when(publicationService.getPublicationByPublicationId(PUBLICATION_ID)).thenReturn(mockPublication);
-//
-//        when(bookService.getBookById(BOOK_ID)).thenReturn(mockBook);
-//
-//        BookModel mockBookModel = mockBook.getBookModel();
-//        when(bookModelService.getBookModelByBookModelId(BOOK_MODEL_ID)).thenReturn(mockBookModel);
-//        
-//        BookImage mockBookImage = new BookImage(BOOK_ID, IMAGE_ORDER, IMAGE_ID, IMAGE_DATETIME);
-//        List<BookImage> mockBookImageList = new ArrayList<>();
-//        mockBookImageList.add(mockBookImage);
-//        
-//        //when(bookImageService.getImageByBookId(BOOK_ID)).thenReturn(mockBookImageList);
-//
-//        Rating mockRating = new Rating(4.5, 10);
-//
-//        when(bookModelService.getBookModelByBookModelId(BOOK_MODEL_ID)).thenReturn(mockBookModel);
-//
-//      //  Publication publicationDetail = publicationDetailService.getPublicationByPublicationId(PUBLICATION_ID);
-//      //  assertNotNull(publicationDetail);
-//        
-//       // assertEquals(mockBookModel, publicationDetail.getBook());
-//       // assertEquals(mockBook, publicationDetail.getBook());
-////        assertEquals(mockBookImageList, publicationDetail.getImages());
-////        assertEquals(mockRating, publicationDetail.getRating());
-//    }
-//}
+
+@RunWith(MockitoJUnitRunner.class)
+public class PublicationServiceImplTest {
+
+    @Mock
+    private UserService userService;
+    @Mock
+    private BookService bookService;
+    @Mock
+    private LocationService locationService;
+    @Mock
+    private PublicationService publicationService;
+    @Mock
+    private PublicationDao publicationDao;
+    
+    @InjectMocks
+    private PublicationServiceImpl publicationDetailService;
+
+    private static final long PUBLICATION_ID = 1;
+    private static final PublicationState PUBLICATION_STATE = PublicationState.CURRENT;
+    private static final long BOOK_ID = 100;
+    private static final long OWNER_ID = 300;
+    private static final long LOCATION_ID = 20;
+    private static final int EXCHANGES_QTY = 5;
+    private static final int RATING = 4;
+    private static final Timestamp IMAGE_DATETIME = new Timestamp(System.currentTimeMillis());
+    
+    @Test
+    public void testCreatePublication() {
+        
+    	Book book = spy(Book.class);
+    	User user = spy(User.class);
+        Location location = spy(Location.class);
+        List<Location> locations = new ArrayList<>();
+        locations.add(location);
+        Publication publication = spy(Publication.class);
+        
+        book.setBookId(BOOK_ID);
+        user.setUserId(OWNER_ID);
+        location.setLocationId(LOCATION_ID);
+        publication.setPublicationState(PUBLICATION_STATE);
+        publication.setPublicationId(PUBLICATION_ID);
+        
+        when(bookService.getBookById(BOOK_ID)).thenReturn(book);
+        when(userService.findById(OWNER_ID)).thenReturn(user);
+        when(locationService.findById(LOCATION_ID)).thenReturn(location);
+        when(publicationDao.createPublication(book, user, locations, PUBLICATION_STATE)).thenReturn(publication);
+
+        Publication publicationDetail = publicationDetailService.createPublication(BOOK_ID, OWNER_ID, LOCATION_ID, PUBLICATION_STATE);
+        
+        Assert.assertNotNull(publicationDetail);
+    }
+    
+    @Test(expected = UserNotUnauthorizedException.class)
+    public void testAddLocationUserNotUnauthorized() {
+
+    	User user_1 = spy(User.class);
+    	User user_2 = spy(User.class);
+    	Location location = spy(Location.class);
+    	Publication publication = spy(Publication.class);
+    	
+    	user_1.setUserId(OWNER_ID);
+    	user_2.setUserId(OWNER_ID + 1);
+    	location.setLocationId(LOCATION_ID);
+    	publication.setUser(user_1);
+    	publication.setPublicationId(PUBLICATION_ID);
+    	
+    	when(locationService.findById(LOCATION_ID)).thenReturn(location);
+    	when(publicationDao.getPublicationByPublicationId(PUBLICATION_ID)).thenReturn(Optional.ofNullable(publication));
+    	
+    	publicationDetailService.addLocation(publication.getPublicationId(), location.getLocationId(), user_2);
+    }
+
+    @Test(expected = PublicationNotFoundException.class)
+    public void testPublicationNotFoundException() {
+    	
+    	when(publicationDao.getPublicationByPublicationId(PUBLICATION_ID)).thenReturn(Optional.ofNullable(null));
+    	
+    	publicationDetailService.getPublicationByPublicationId(PUBLICATION_ID);
+    }
+}
--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -85,18 +85,4 @@ public class UserServiceImplTest {
 	
 	    userService.createUser(USER_NAME, MAIL, PASSWORD, LANGUAGE);
 	}
-//    User findById(long id);
-//    User createUser(String username, String mail, String password, String language);
-//    Optional<User> findByUsername(String username);
-//    void verifyUser(int verificationCode);
-//    void changePasswordSolicited(String email);
-//    void changePassword(int verificationCode, String newPassword);
-//    boolean userExists(String mail);
-//    boolean changeUserName(long userId, String newName);
-//    User getUserToVerify(int verificationCode);
-//    void setUserLanguage(User user, String language);
-//    void addLocation(Long userId, String locationString);
-//    void removeLocation(Long userId, Long locationId);
-	
-	
 }
