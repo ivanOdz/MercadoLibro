0f1a423380923155f886d1e1729d385dd152f4dc
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-11-10 21:35:40 -0300

    Deleted unused imports

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -1,9 +1,5 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import ar.edu.itba.paw.interfaces.exceptions.base.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
