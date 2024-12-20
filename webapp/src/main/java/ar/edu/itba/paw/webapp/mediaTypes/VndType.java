c2f7f3f19abd4240e2b9aa58a8485dd069a5b12b
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-12-20 11:58:38 -0300

    CHORE vnd types file

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/mediaTypes/VndType.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.paw.webapp.mediaTypes;
+
+public class VndType {
+    public VndType() {
+        throw new AssertionError();
+    }
+
+    public static final String APPLICATION_USER = "application/vnd.users.v1+json";
+    public static final String APPLICATION_BOOK = "application/vnd.users.v1+json";
+    public static final String APPLICATION_PUBLICATION = "application/vnd.users.v1+json";
+    public static final String APPLICATION_EXCHANGE = "application/vnd.users.v1+json";
+}
