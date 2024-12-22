17bb75cc3d6c31aca83666b8e2866caedd95bd4e
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-12-21 22:51:13 -0300

    CHORE: Finished implementation of basic custom authentication filter

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/BasicAuthTokenIssuerFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/BasicAuthTokenIssuerFilter.java
@@ -1,26 +1,22 @@
 package ar.edu.itba.paw.webapp.auth;
 
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.crypto.Ed25519Signer;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.Date;
-import org.springframework.security.oauth2.jwt.Jwt;
 
 @Component
 public class BasicAuthTokenIssuerFilter extends BasicAuthenticationFilter {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     public BasicAuthTokenIssuerFilter(AuthenticationManager authenticationManager, AuthenticationEntryPoint authenticationEntryPoint) {
         super(authenticationManager, authenticationEntryPoint);
@@ -29,37 +25,7 @@ public class BasicAuthTokenIssuerFilter extends BasicAuthenticationFilter {
     @Override
     protected void onSuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, Authentication authResult) throws IOException {
         super.onSuccessfulAuthentication(request, response, authResult);
-        final String jwt;
-        try {
-            jwt = createJwt(authResult);
-        } catch (Exception e) {
-            // To call
-            throw new IOException();
-        }
-        response.addHeader("Authorization", "Bearer " + jwt);
+        String jwt = jwtTokenUtil.createToken(authResult);
+        response.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwt);
     }
-
-    public String createJwt(Authentication authentication) throws Exception {
-
-        // Build the claims set
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .subject(authentication.getName())
-                .expirationTime(new Date(System.currentTimeMillis() + 3600 * 1000)) // Valid for 1 hour
-                .claim("roles", authentication.getAuthorities().stream()
-                        .map(auth -> auth.getAuthority())
-                        .toList())
-                .build();
-
-        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256).build();
-
-        SignedJWT signedJWT = new SignedJWT(header, claimsSet);
-
-        // Sign it with the secret key
-        // This sign throws
-        signedJWT.sign(new MACSigner("secretKey"));
-
-        // Serialize it to a compact token string
-        return signedJWT.serialize();
-    }
-
 }
