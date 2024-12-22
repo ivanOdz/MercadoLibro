61e1eac46e4d302422b12d6fe20a04ba23a3c534
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-12-21 22:00:30 -0300

    CHORE: Create JWT with secret 256 bit key shh

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtTokenUtil.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtTokenUtil.java
@@ -1,50 +1,55 @@
 package ar.edu.itba.paw.webapp.auth;
 
+import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.models.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
+import org.springframework.util.FileCopyUtils;
+import javax.crypto.SecretKey;
+import java.io.IOException;
 import java.util.Date;
+
 
 @Component
 public class JwtTokenUtil {
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+    @Autowired
+    private UserService userService;
 
-    @Value("${jwt.expiration}")
-    private long expirationTime = 3600000; // 1 hour by default
+    @Value("classpath:jwt.key")
+    private Resource jwtKeyResource;
 
-    // Method to generate JWT token
-    public String generateToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+    private final SecretKey jwtSigningKey;
+
+    private static final int EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000; //1 week (in millis)
+
+    public JwtTokenUtil() throws IOException {
+        byte[] keyBytes = FileCopyUtils.copyToByteArray(jwtKeyResource.getInputStream());
+        this.jwtSigningKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(Authentication userAuth) {
+        PawUserDetails pud = (PawUserDetails) userAuth.getPrincipal();
+        User user = userService.findById(pud.getUser().getUserId());
+
+        Claims claims = Jwts.claims();
+
+        claims.setSubject(user.getUsername());
+
+        //claims.put("authorization", user.getRole());
+        return "Bearer " + Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(jwtSigningKey, SignatureAlgorithm.HS256)
                 .compact();
-    }
-
-
-    // Method to validate JWT token
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    // Method to extract username from JWT token
-    public String getUsername(String token) {
-        return Jwts.parser()
-                .setSigningKey(secretKey)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
     }
 
 }
