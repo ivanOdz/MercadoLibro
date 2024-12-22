95c2caf496ab0cc0b530da1e8f49459a717694bb
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2024-12-21 23:37:01 -0300

    CHORE: refactor JwtAuthenticationFilter: enhanced token validation

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtAuthenticationFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,30 +24,38 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
+    @Autowired
+    private PawUserDetailsService pawUserDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if(header == null || !header.startsWith("Bearer ")) {
+        String username = jwtTokenUtil.parseToken(header);
+
+        if (username == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        final String token = header.substring(7);   // Remove "Bearer "
-        PawUserDetails pawUserDetails = jwtTokenUtil.parseAndValidateToken(token);
-
-        if(pawUserDetails == null) {
+        PawUserDetails pawUserDetails;
+        try {
+            pawUserDetails = (PawUserDetails) pawUserDetailsService.loadUserByUsername(username);
+        } catch (UsernameNotFoundException e){
             filterChain.doFilter(request, response);
             return;
         }
 
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                pawUserDetails.getUser().getUsername(), pawUserDetails.getUser().getPassword(), pawUserDetails.getAuthorities());
+                pawUserDetails.getUser().getUsername(),
+                pawUserDetails.getUser().getPassword(),
+                pawUserDetails.getAuthorities()
+        );
 
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
         filterChain.doFilter(request, response);
     }
 }
