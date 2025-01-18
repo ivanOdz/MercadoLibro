5266abc43d1b6cc34f6fb528f2912c1f065d00a5
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-01-18 18:05:07 -0300

    CHORE genre summary bookModel

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/BookModelController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/BookModelController.java
@@ -3,15 +3,17 @@ package ar.edu.itba.paw.webapp.controller;
 import ar.edu.itba.paw.interfaces.services.BookModelService;
 import ar.edu.itba.paw.interfaces.services.BookService;
 import ar.edu.itba.paw.models.*;
-import ar.edu.itba.paw.models.utils.BookDimension;
-import ar.edu.itba.paw.models.utils.Genre;
-import ar.edu.itba.paw.models.utils.Language;
+import ar.edu.itba.paw.models.utils.*;
 import ar.edu.itba.paw.models.utils.pagination.BookModelMetadata;
 import ar.edu.itba.paw.webapp.dto.Book.AuthorDTO;
 import ar.edu.itba.paw.webapp.dto.input.BookModelDTO;
+import ar.edu.itba.paw.webapp.dto.output.BookConditionDTO;
+import ar.edu.itba.paw.webapp.dto.output.GenreDTO;
 import ar.edu.itba.paw.webapp.mediaTypes.VndType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
@@ -31,25 +33,6 @@ public class BookModelController {
     @Context
     private UriInfo uriInfo;
 
-    /*
-    @RequestMapping("/book/book_models")
-    public ModelAndView bookModels(@RequestParam(name = "search", defaultValue = "") String search,
-                                   @RequestParam(name = "is-genre-filter-active", defaultValue = "false") String isGenreFilterActive,
-                                   @RequestParam(name = "genre-filter", required = false) String genreFilter,
-                                   @RequestParam(name = "page", defaultValue = "0") int currentPage,
-                                   @RequestParam(name = "sort-type", defaultValue = "BOOK_NAME_ASCENDING") String sortType) {
-
-        ModelAndView mav = new ModelAndView("book/book_models");
-        PaginatedResponse<BookModel, BookModelMetadata> modelBooks = bookModelService.getPaginatedBookModels(search, isGenreFilterActive, genreFilter, currentPage, sortType);
-
-        List<GenreWrapper> genreWrapperList = bookModelService.getGenreWrapperList(search);
-
-        mav.addObject("genres", genreWrapperList);
-        mav.addObject("modelBooks", modelBooks);
-
-        return mav;
-    }*/
-
     @GET
     @Produces(value = {VndType.APPLICATION_BOOK_MODEL})
     public Response getBookModels(@QueryParam("search") @DefaultValue("")final String search,
@@ -63,20 +46,6 @@ public class BookModelController {
 
         return Response.ok(new GenericEntity<List<BookModelDTO>>(bookModels) {}).build();
     }
-
-
-    /*
-    @PostMapping("/book/create_book")
-    public ModelAndView createBook(@Valid @ModelAttribute(name = "bookDetailsForm") BookDetailsForm bookDetailsForm, BindingResult errors, @RequestParam("book_model_id") Long bookModelId, @ModelAttribute("loggedUser") User loggeduser) {
-        if (errors.hasErrors()) {
-            return bookDetailsFormNewBook(bookDetailsForm, bookModelId, errors, loggeduser);
-        }
-        Book book = bookService.createBook(bookModelId, bookDetailsForm.getBookState(), bookDetailsForm.getRating(), bookDetailsForm.getImageFiles(), bookDetailsForm.getBookCover(), null, loggeduser, false);
-
-        publicationService.createPublicationIfNeeded(bookDetailsForm.isPublish(), book.getBookId(), loggeduser.getUserId(), bookDetailsForm.getLocationId(), PublicationState.CURRENT);
-        return new ModelAndView("redirect:/book");
-    }*/
-
 
     @POST
     @Consumes(value = {VndType.APPLICATION_BOOK_MODEL})  // /book_models
@@ -99,5 +68,13 @@ public class BookModelController {
     public Response setBookCover(@PathParam("id") Long bookModelId, @QueryParam("image-id") Long imageId) {
         BookModel bookModel = bookModelService.setCover(bookModelId, imageId);
         return Response.created(uriInfo.getAbsolutePathBuilder().path(String.valueOf(bookModel.getBookModelId())).build()).build();
+    }
+
+    @GET
+    @Path("/genres-summary")
+    public Response getGenresSummary(@QueryParam("search") String search){
+        List<GenreWrapper> genresSummary = bookModelService.getGenreWrapperList(search);
+        List<GenreDTO> genres = genresSummary.stream().map(g -> GenreDTO.fromGenreWrapper(uriInfo, g)).toList();
+        return Response.ok(new GenericEntity<List<GenreDTO>>(genres) {}).build();
     }
 }
