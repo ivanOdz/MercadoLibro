09001f8f763bd9801fad55d23abab436ffe16583
Maggie Taurian <mtaurian@itba.edu.ar>
2025-01-30 16:31:55 -0300

    FIX: updateUser callings

--- services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/UserServiceImpl.java
@@ -56,8 +56,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public User updateUser(String username, String language, String newUsername) {
-        User user = userDao.findByUsername(username).orElseThrow(() -> new UserNotFoundException("No such user"));
+    public User updateUser(long userId, String language, String newUsername) {
+        User user = userDao.findById(userId).orElseThrow(() -> new UserNotFoundException("No such user"));
         if(language != null){
             user = setUserLanguage(user.getUserId(), language);
         }
