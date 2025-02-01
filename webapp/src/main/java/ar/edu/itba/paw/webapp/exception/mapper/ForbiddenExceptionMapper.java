7822b846ca5eeb4f4428bb511aebea829b1aff6f
tscheffer1 <tscheffer@itba.edu.ar>
2025-02-01 15:59:36 -0300

    CHORE: Base for Forbidden exception

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exception/mapper/ForbiddenExceptionMapper.java
@@ -0,0 +1,25 @@
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import ar.edu.itba.paw.interfaces.exceptions.base.ForbiddenException;
+import ar.edu.itba.paw.webapp.dto.output.ErrorDTO;
+
+public class ForbiddenExceptionMapper implements ExceptionMapper<ForbiddenException> {
+	
+	@Context
+    private UriInfo uriInfo;
+	
+	@Override
+	public Response toResponse(ForbiddenException exception) {
+		
+		ErrorDTO errorDTO = ErrorDTO.fromErrorDTO(uriInfo, exception.getMessage());
+		
+		return Response.status(Response.Status.BAD_REQUEST)
+						.entity(errorDTO)
+						.type(MediaType.APPLICATION_JSON)
+						.build();
+	}
+}
