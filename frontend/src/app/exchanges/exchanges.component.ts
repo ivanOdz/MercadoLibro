3b073a05f372812f66030d198bfc92daa5674ec7
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-01 17:52:35 -0300

    CHORE: added exchanges component

---
+++ frontend/src/app/exchanges/exchanges.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exchanges',
+  imports: [],
+  template: `
+    <p>
+      exchanges works!
+    </p>
+  `,
+  styleUrl: './exchanges.component.css'
+})
+export class ExchangesComponent {
+
+}
