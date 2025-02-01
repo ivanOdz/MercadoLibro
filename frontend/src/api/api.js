0a5d97304fd56d72afff4764e7ba468b0cb36f30
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-01 19:52:16 -0300

    FIX: corrected baseUrl

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     timeout: 5000,
-    baseURL: process.env.PUBLIC_URL,
+    baseURL: process.env.PUBLIC_URL + '/api',
     headers: {
         Accept: 'application/json',
     },
