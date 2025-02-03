a354e5f0eeeeaa9a4dc356c8690458b05a4c3360
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-03 18:57:45 -0300

    SETUP primeng and navbar

--- frontend/src/app/features/exchanges/exchanges.component.ts
+++ frontend/src/app/features/exchanges/exchanges.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-exchanges',
   templateUrl: `exchanges.component.html`,
   standalone: true,
   styleUrl: './exchanges.component.css',
-  imports: [NavbarComponent]
+  imports: [NavbarComponent, ButtonModule]
 })
 export class ExchangesComponent {
 
