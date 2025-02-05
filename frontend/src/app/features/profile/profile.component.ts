5bdfd81202b663518ebf6c92d7c6d3b83f2e2c16
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-05 20:10:27 -0300

    CHORE: profile basics

--- frontend/src/app/features/profile/profile.component.ts
+++ frontend/src/app/features/profile/profile.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import {Component, inject} from '@angular/core';
+import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+import {NgForOf, NgOptimizedImage} from '@angular/common';
+import { User } from '../../core/models/user.model';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [
-    NavbarComponent
-  ],
+  imports: [NavbarComponent, NgOptimizedImage, NgForOf],
   standalone: true,
-  templateUrl: `./profile.component.html`,
+  templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  //user: User;
+  userServices: UserService = inject(UserService);
+
+  constructor() {
+    //this.user = this.userServices.getUser();
+  }
 
 }
+
