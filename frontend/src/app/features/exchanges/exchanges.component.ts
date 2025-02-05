a9ea87a78e851d411ec65cbbe1c1764eceafab9b
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-05 20:22:35 -0300

    CHORE exchange active card right side frontend

--- frontend/src/app/features/exchanges/exchanges.component.ts
+++ frontend/src/app/features/exchanges/exchanges.component.ts
@@ -5,6 +5,8 @@ import {Title} from "@angular/platform-browser";
 import {SidebarComponent} from "./components/sidebar.component";
 import {NgForOf} from "@angular/common";
 import {Paginator, PaginatorState} from "primeng/paginator";
+import {Steps} from "primeng/steps";
+import {MenuItem} from "primeng/api";
 
 
 @Component({
@@ -12,7 +14,7 @@ import {Paginator, PaginatorState} from "primeng/paginator";
   templateUrl: `exchanges.component.html`,
   standalone: true,
   styleUrl: './exchanges.component.css',
-  imports: [ButtonModule, SidebarComponent, NavbarComponent, NgForOf, Paginator]
+  imports: [ButtonModule, SidebarComponent, NavbarComponent, NgForOf, Paginator, Steps]
 })
 export class ExchangesComponent {
 
@@ -32,10 +34,19 @@ export class ExchangesComponent {
   rows: unknown;
   totalRecords: unknown;
   first: number;
-
+  steps: MenuItem[] = [
+    { label: 'Aceptado' },
+    { label: 'Esperando confirmacion' },
+    { label: 'Finalizado' }
+  ];
 
     constructor(private titleService: Title) {
         this.titleService.setTitle(this.Title);
         this.first = 1;
     }
+
+
+  confirmExchange(card: string) {
+
+  }
 }
