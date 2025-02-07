faf31dddcc84e1220b4428a9371b0024502086b6
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-07 01:44:26 -0300

    CHORE: created Review model

---
+++ frontend/src/app/core/models/review.model.ts
@@ -0,0 +1,19 @@
+export class Review {
+  description: string;
+  reviewDate: Date; // Date
+  rating: number;
+  self: string;
+  subject: string;
+  reviewer: string;
+  exchange: string;
+
+  constructor(data: any) {
+    this.description = data.description;
+    this.reviewDate = data.reviewDate;
+    this.rating = data.rating;
+    this.self = data.self;
+    this.subject = data.subject;
+    this.reviewer = data.reviewer;
+    this.exchange = data.exchange;
+  }
+}
