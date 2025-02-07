530f2a6f8314fbc30b89b04167d2bf7cdbdbd296
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2025-02-07 03:25:32 -0300

    CHORE: Request password change and change password functionalities.

---
+++ frontend/src/app/features/forgot-password/forgot-password.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {InputText} from "primeng/inputtext";
+import {ButtonDirective} from "primeng/button";
+import {UserService} from "../../core/services/user.service";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-forgot-password',
+  imports: [
+    FormsModule,
+    InputText,
+    ButtonDirective,
+    NgIf
+  ],
+  standalone: true,
+  templateUrl: './forgot-password.component.html',
+  styleUrl: './forgot-password.component.css'
+})
+export class ForgotPasswordComponent {
+  email: string = '';
+  message: string = '';
+  showConfirmation: boolean = false;
+  showForm: boolean = true;
+
+  constructor(private userService: UserService) {}
+
+  sendRecoveryCode() {
+    this.userService.changePasswordRequest(this.email).subscribe({
+      next: ()=> {
+        this.showForm = false;
+        this.showConfirmation = true;
+        this.message = 'Si el correo está registrado, revisa tu bandeja de entrada para el código de recuperación.';
+      },
+      error: () => {
+        this.showForm = false;
+        this.showConfirmation = true;
+        this.message = 'Si el correo está registrado, revisa tu bandeja de entrada para el código de recuperación.';
+    }
+    });
+  }
+}
