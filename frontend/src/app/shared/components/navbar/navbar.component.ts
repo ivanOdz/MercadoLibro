d1bf97547ceac9999accc0a33da9f6b5017dadd7
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2025-02-07 23:34:39 -0300

    CHORE: Added translations to tieredmenu

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -8,10 +8,10 @@ import { InputText } from 'primeng/inputtext';
 import { MenuItem } from 'primeng/api';
 import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
 import { TieredMenu } from 'primeng/tieredmenu';
-import {RouterLink} from "@angular/router";
-import {TranslatePipe} from "@ngx-translate/core";
-import {AuthService} from "../../../core/services/auth.service";
-import {NgOptimizedImage} from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
+import { AuthService } from "../../../core/services/auth.service";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
     selector: 'app-navbar',
@@ -33,17 +33,28 @@ import {NgOptimizedImage} from "@angular/common";
 })
 export class NavbarComponent {
 
-    constructor(private authService: AuthService) {}
     @ViewChild('profileMenu') profileMenu!: TieredMenu;
+    profileItems: MenuItem[] = [];
 
-    profileItems: MenuItem[] = [
-        { label: 'Profile',  routerLink: '/profile' },
-        { label: 'My Books',  routerLink: '/my-books' },
-        { label: 'My Publications',  routerLink: '/my-publications' },
-        { label: 'Favorites',  routerLink: '/favorites' },
-        { separator: true },
-        { label: 'Logout', icon: 'pi pi-sign-out', command: () => this.logout() }
-    ];
+    constructor(private authService: AuthService, private translate: TranslateService) {
+        this.translate.onLangChange.subscribe(() => this.loadProfileItems());
+        this.loadProfileItems();
+    }
+
+    loadProfileItems() {
+        this.profileItems = [
+            { label: this.translate.instant('NAVBAR.PROFILE'), routerLink: '/profile' },
+            { label: this.translate.instant('NAVBAR.MY_BOOKS'), routerLink: '/my-books' },
+            { label: this.translate.instant('NAVBAR.MY_PUBLICATIONS'), routerLink: '/my-publications' },
+            { label: this.translate.instant('NAVBAR.FAVORITES'), routerLink: '/favorites' },
+            { separator: true },
+            {
+                label: this.translate.instant('NAVBAR.LOGOUT'),
+                icon: 'pi pi-sign-out',
+                command: () => this.logout()
+            }
+        ];
+    }
 
     toggleProfileMenu(event: Event) {
         this.profileMenu.toggle(event);
