07b79ebe64fe43feeaeae2781a26643b1a4a6ce6
tscheffer1 <tscheffer@itba.edu.ar>
2025-02-08 04:56:23 -0300

    CHORE: New book form component

---
+++ frontend/src/app/features/book-form/book-form.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "../../shared/components/navbar/navbar.component";
+
+@Component({
+	selector: 'app-book-form',
+	standalone: true,
+	imports: [CommonModule, ReactiveFormsModule, NavbarComponent],
+	templateUrl: './book-form.component.html',
+	styleUrls: ['./book-form.component.css'],
+})
+export class BookFormComponent {
+	bookForm: FormGroup;
+	
+	constructor(private formBuilder: FormBuilder) {
+		this.bookForm = this.formBuilder.group({
+													isbn: ['', Validators.required],
+													title: ['', Validators.required],
+													editorial: ['', Validators.required],
+													description: [''],
+													genre: ['', Validators.required],
+													edition: [1, [Validators.required, Validators.min(1)]],
+													weight: [0, [Validators.required, Validators.min(0)]],
+													pages: [1, [Validators.required, Validators.min(1)]],
+													bookLanguage: ['', Validators.required],
+													dimension: [''],
+													publicationYear: [2025, [Validators.required, Validators.min(999)]],
+													isPocketEdition: [false],
+													isHardcover: [false],
+													ratingCount: [0, Validators.min(0)],
+													averageRating: [0, [Validators.min(0), Validators.max(5)]],
+													authors: this.formBuilder.array([]),
+												});
+	}
+
+	get authors(): FormArray {
+		return this.bookForm.get('authors') as FormArray;
+	}
+	
+	addAuthor() {
+		this.authors.push(this.formBuilder.control(''));
+	}
+	
+	removeAuthor(index: number) {
+		this.authors.removeAt(index);
+	}
+	
+	submitForm() {
+		if (this.bookForm.valid) {
+			console.log('New book:', this.bookForm.value);
+		}
+	}
+}
