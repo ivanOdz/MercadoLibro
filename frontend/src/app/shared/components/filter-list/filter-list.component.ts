f16eb18c5ea18df030c96f9cce6d72b0d6aebb58
ivanOdz <76888044+ivanOdz@users.noreply.github.com>
2025-02-08 21:18:51 -0300

    CHORE: Filtering component

---
+++ frontend/src/app/shared/components/filter-list/filter-list.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {NgForOf, UpperCasePipe} from "@angular/common";
+import {Card} from "primeng/card";
+import {PrimeTemplate} from "primeng/api";
+
+@Component({
+  selector: 'app-filter-list',
+  templateUrl: './filter-list.component.html',
+  standalone: true,
+  imports: [
+    UpperCasePipe,
+    TranslatePipe,
+    Card,
+    PrimeTemplate,
+    NgForOf
+  ],
+  styleUrls: ['./filter-list.component.css']
+})
+export class FilterListComponent implements OnInit {
+  @Input() headers: Record<string, string> = {};
+  @Input() filterType: 'Condition' | 'Genre' = 'Condition';
+  @Output() filterSelected = new EventEmitter<string>();
+
+  filters: { labelKey: string; count: number; queryParam: string }[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.parseHeaders();
+  }
+
+  parseHeaders() {
+    const prefix = this.filterType === 'Condition' ? 'bookstate.' : 'genre.';
+    const paramName = this.filterType === 'Condition' ? 'state' : 'genre';
+
+    this.filters = Object.entries(this.headers)
+        .map(([key, value]) => {
+          const match = value.match(new RegExp(`${prefix}(.*?)=(\\d+)`));
+          if (!match) return null;
+
+          const valueKey = match[1];
+          const formattedLabelKey = `${prefix}${valueKey}`;
+          const count = parseInt(match[2], 10);
+          const queryParam = `${paramName}=${formattedLabelKey}`;
+
+          return { labelKey: formattedLabelKey, count, queryParam };
+        })
+        .filter(filter => filter !== null) as { labelKey: string; count: number; queryParam: string }[];
+  }
+
+  onFilterClick(queryParam: string) {
+    this.filterSelected.emit(queryParam);
+  }
+}
