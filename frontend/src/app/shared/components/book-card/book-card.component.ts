32dc4af28b62955d5851412ec26dc1fb6c13491e
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-09 21:38:39 -0300

    FIX import warning

--- frontend/src/app/shared/components/book-card/book-card.component.ts
+++ frontend/src/app/shared/components/book-card/book-card.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Book } from './../../../core/models/book.model';
+import {NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-book-card',
-  templateUrl: './book-card.component.html',
-  styleUrl: './book-card.component.css',
-  standalone: true
+    selector: 'app-book-card',
+    templateUrl: './book-card.component.html',
+    styleUrl: './book-card.component.css',
+    imports: [
+        NgIf
+    ],
+    standalone: true
 })
 export class BookCardComponent {
 	@Input() book!: Book;
