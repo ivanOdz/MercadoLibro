15b75cbcf7a9ba4e47324bf91b33915a3a587967
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-10 18:41:12 -0300

    CHORE publciation detail carousel

--- frontend/src/app/features/publication-detail/publication.component.ts
+++ frontend/src/app/features/publication-detail/publication.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from "@angular/core";
 import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
 import {TranslatePipe, TranslateService} from "@ngx-translate/core";
 import {Button} from "primeng/button";
+import { Carousel } from 'primeng/carousel';
+import {NgOptimizedImage} from "@angular/common";
+import {PrimeTemplate} from "primeng/api";
+import {routes} from "../../app.routes";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-publication-detail',
@@ -10,7 +15,9 @@ import {Button} from "primeng/button";
     imports: [
         NavbarComponent,
         TranslatePipe,
-        Button
+        Button,
+        Carousel,
+        PrimeTemplate
     ],
     styleUrls: ['./publication.component.css']
 })
@@ -19,7 +26,7 @@ export class PublicationComponent implements OnInit {
     ngOnInit(): void {
     }
 
-    constructor(private translate: TranslateService) {
+    constructor(private translate: TranslateService, private router: Router) {
         this.translate.onLangChange.subscribe(() => this.loadPublicationDetailItems());
     }
 
@@ -27,4 +34,21 @@ export class PublicationComponent implements OnInit {
         // static data translations
     }
 
+
+    bookImages: string[] = [
+        'http://localhost:8080/assets/book.jpg',
+        'http://localhost:8080/assets/book.jpg',
+        'http://localhost:8080/assets/book.jpg',
+        'http://localhost:8080/assets/book.jpg',
+        'http://localhost:8080/assets/book.jpg',
+        'http://localhost:8080/assets/book.jpg',
+        'http://localhost:8080/assets/book.jpg'
+    ];
+
+    protected readonly routes = routes;
+    protected readonly Router = Router;
+
+    goToPublications() {
+        this.router.navigate(['/publications']);
+    }
 }
