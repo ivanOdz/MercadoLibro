8e8b7659c8f8674917c5aa8f45f56918fcdcb7e4
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-10 15:18:51 -0300

    CHORE pubcliation detail front main book card

--- frontend/src/app/features/publication-detail/publication.component.ts
+++ frontend/src/app/features/publication-detail/publication.component.ts
@@ -1,18 +1,30 @@
 import {Component, OnInit} from "@angular/core";
 import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import {TranslatePipe, TranslateService} from "@ngx-translate/core";
+import {Button} from "primeng/button";
 
 @Component({
     selector: 'app-publication-detail',
     templateUrl: `./publication.component.html`,
     standalone: true,
     imports: [
-        NavbarComponent
+        NavbarComponent,
+        TranslatePipe,
+        Button
     ],
     styleUrls: ['./publication.component.css']
 })
 export class PublicationComponent implements OnInit {
 
     ngOnInit(): void {
+    }
+
+    constructor(private translate: TranslateService) {
+        this.translate.onLangChange.subscribe(() => this.loadPublicationDetailItems());
+    }
+
+    loadPublicationDetailItems() {
+        // static data translations
     }
 
 }
