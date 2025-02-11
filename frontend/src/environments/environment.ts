ddc3f6257170d545d13c12265801ab00b08e04b7
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-11 01:29:53 -0300

    CHORE basepath fixed

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    productionUrl: 'http://localhost:8080/api',
-    developmentUrl: 'http://pawserver.it.itba.edu.ar/paw-2024b-09/api',
+    production: true,
+    productionUrl: 'http://pawserver.it.itba.edu.ar/paw-2024b-09/api',
+    developmentUrl: 'http://localhost:8080/api',
 };
