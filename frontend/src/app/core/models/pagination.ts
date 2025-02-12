7e8f31fc66fc85cfc5c2010bee2e548eeae1af4f
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-12 00:23:01 -0300

    FIX: pagination model

--- frontend/src/app/core/models/pagination.ts
+++ frontend/src/app/core/models/pagination.ts
@@ -1,8 +1,8 @@
 export class Pagination {
-    next: string | undefined
-    prev: string | undefined
-    last: string | undefined
-    first: string | undefined
+    next: string | undefined;
+    prev: string | undefined;
+    last: string | undefined;
+    first: string | undefined;
 
     constructor(linksHeader: string | null) {
         if (linksHeader) {
@@ -12,12 +12,24 @@ export class Pagination {
                 if (match) {
                     const url = match[1];
                     const rel = match[2];
-                    if (rel === 'first') this.first = url;
-                    if (rel === 'prev') this.prev = url;
-                    if (rel === 'next') this.next = url;
-                    if (rel === 'last') this.last = url;
+                    if (rel === 'first') this.first = url || undefined;
+                    if (rel === 'prev') this.prev = url || undefined;
+                    if (rel === 'next') this.next = url || undefined;
+                    if (rel === 'last') this.last = url || undefined;
                 }
             });
         }
+    }
+
+    hasPrev(): boolean {
+        return !!this.prev;
+    }
+
+    hasNext(): boolean {
+        return !!this.next;
+    }
+
+    multiplePages(): boolean {
+        return this.first != this.last;
     }
 }
