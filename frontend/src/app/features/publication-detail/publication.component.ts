6bf8a2728ab7e5bbdf5e15662443b0a6b73cedf3
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-12 15:07:59 -0300

    CHORE publication detail data

--- frontend/src/app/features/publication-detail/publication.component.ts
+++ frontend/src/app/features/publication-detail/publication.component.ts
@@ -7,13 +7,22 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import {PrimeTemplate} from "primeng/api";
 import {routes} from "../../app.routes";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {NgForOf, NgIf} from "@angular/common";
 import {Select} from "primeng/select";
 import {FormsModule} from "@angular/forms";
 import {environment} from "../../../environments/environment";
 import {Divider} from "primeng/divider";
 import {Paginator} from "primeng/paginator";
+import {PublicationData2} from "../../core/models/types";
+import {PublicationService} from "../../core/services/publication.service";
+import {catchError, forkJoin, of, switchMap, tap} from "rxjs";
+import {map} from "rxjs/operators";
+import {UserService} from "../../core/services/user.service";
+import {BookService} from "../../core/services/book.service";
+import {BookModelService} from "../../core/services/bookmodel.service";
+import {User} from "../../core/models/user.model";
+import {BookModel} from "../../core/models/bookModel.model";
 
 @Component({
     selector: 'app-publication-detail',
@@ -44,40 +53,99 @@ import {Paginator} from "primeng/paginator";
             ])
         ]),
         trigger('fadeOutIn', [
-                transition(':leave', [
-                    animate('500ms ease-in', style({ opacity: 0, transform: 'translateY(20px)' }))
-                ]),
-                transition(':enter', [
-                    style({ opacity: 0, transform: 'translateY(-20px)' }),
-                    animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-                ]),
+            transition(':leave', [
+                animate('500ms ease-in', style({ opacity: 0, transform: 'translateY(20px)' }))
+            ]),
+            transition(':enter', [
+                style({ opacity: 0, transform: 'translateY(-20px)' }),
+                animate('500ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+            ]),
         ])
     ],
     styleUrls: ['./publication.component.css']
 })
 export class PublicationComponent implements OnInit {
 
+    publication: PublicationData2 | null = null;
+    bookImages: string[] = [];
+    publicationLocations: string[] = [];
+
     ngOnInit(): void {
+        this.route.params.subscribe(params => {
+            const id = params['id'];
+            this.loadPublication(id);
+        });
     }
 
-    constructor(private translate: TranslateService, private router: Router) {
+    loadPublication(id: string) {
+        this.ps.getPublication(`${environment.production ? environment.productionUrl : environment.developmentUrl}/publications/${id}`).pipe(
+            switchMap((publication) => {
+                if (!publication) {
+                    throw new Error('No se encontró la publicación');
+                }
+                return forkJoin({
+                    publication: of(publication),
+                    book: this.bs.getBook(publication.book),
+                    user: this.us.getUser(publication.user),
+                    locations: this.us.getLocationsInPublication(publication.locations)
+                });
+            }),
+            switchMap(({ publication, book, user, locations }) => {
+                if (!book) {
+                    throw new Error('No se encontró el libro');
+                }
+                return forkJoin({
+                    publication: of(publication),
+                    book: of(book),
+                    user: of(user),
+                    locations: of(locations),
+                    bookModel: this.bms.getBookModel(book.bookModel)
+                });
+            }),
+            catchError((error) => {
+                console.error(error);
+                return of(null);
+            })
+        ).subscribe((data) => {
+            if (data) {
+                this.bookImages = data.book.images;
+                this.publicationLocations = data.locations.map(location => location.location);
+                this.publication = {
+                    book: {
+                        state: data.book.state,
+                        available: data.book.available,
+                        owner: data.user,
+                        bookModel: data.bookModel,
+                        images: data.book.images,
+                        self: data.book.self
+                    },
+                    locations: data.locations,
+                    user: data.user,
+                    publicationState: data.publication.publicationState,
+                    publicationDatetime: new Date(data.publication.publicationDatetime),
+                    favoriteEndpoint: data.publication.favoriteEndpoint,
+                    favoritePublication: null,
+                    isFavoriteTemplate: '',
+                    self: data.publication.self
+                };
+            }
+        });
+    }
+
+
+    constructor(private translate: TranslateService,
+                private router: Router,
+                private route: ActivatedRoute,
+                private ps: PublicationService,
+                private us: UserService,
+                private bs: BookService,
+                private bms: BookModelService) {
         this.translate.onLangChange.subscribe(() => this.loadPublicationDetailItems());
     }
 
     loadPublicationDetailItems() {
         // static data translations
     }
-
-
-    bookImages: string[] = [
-        `${environment.production ? environment.productionUrl : environment.developmentUrl}/assets/book.jpg`,
-        `${environment.production ? environment.productionUrl : environment.developmentUrl}/assets/book.jpg`,
-        `${environment.production ? environment.productionUrl : environment.developmentUrl}/assets/book.jpg`,
-        `${environment.production ? environment.productionUrl : environment.developmentUrl}/assets/book.jpg`,
-        `${environment.production ? environment.productionUrl : environment.developmentUrl}/assets/book.jpg`,
-        `${environment.production ? environment.productionUrl : environment.developmentUrl}/assets/book.jpg`,
-        `${environment.production ? environment.productionUrl : environment.developmentUrl}/assets/book.jpg`,
-    ];
 
     books = [
         { title: "Libro 1" }, { title: "Libro 2" }, { title: "Libro 3" },
