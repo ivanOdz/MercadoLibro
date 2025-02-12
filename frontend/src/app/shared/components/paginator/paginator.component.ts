cb7619be56bc2c25af85e9c06f31bd80a6df877b
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-11 23:55:27 -0300

    CHORE: created paginator component

---
+++ frontend/src/app/shared/components/paginator/paginator.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Pagination } from "../../../core/models/pagination";
+import { NgIf } from "@angular/common";
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-paginator',
+  imports: [ NgIf, MatIconModule ],
+  templateUrl: './paginator.component.html',
+  styleUrl: './paginator.component.css'
+})
+export class PaginatorComponent {
+  @Input() fetchMethod!: (url: string) => void;
+  @Input() pagination: Pagination | null = null;
+
+  fetch(url: string | undefined) {
+    if (url && this.fetchMethod) {
+      this.fetchMethod(url);
+    }
+  }
+
+}
