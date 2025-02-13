729e8824fc1b9e45ad9291fa2a2eb41955548cda
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-02-13 20:50:51 -0300

    CHORE publication cards redirect

--- frontend/src/app/shared/publication-card/publication.card.ts
+++ frontend/src/app/shared/publication-card/publication.card.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from "@angular/core";
 import { PublicationData} from "../../core/models/types";
+import { Router} from "@angular/router";
 
 @Component({
     selector: 'publication-card',
@@ -9,4 +10,20 @@ import { PublicationData} from "../../core/models/types";
 })
 export class PublicationCardComponent {
     @Input() publication!: PublicationData;
+
+    constructor(private router: Router) {
+    }
+
+    getBookImage(images: string[] | null | undefined) {
+        return images? images[0] : 'assets/book.jpg';
+    }
+
+
+    goToPublicationDetail() {
+        if (this.publication.self) {
+            const path = this.publication.self.replace(/^.*\/api/, '');
+            this.router.navigate([path]);
+        }
+    }
+
 }
