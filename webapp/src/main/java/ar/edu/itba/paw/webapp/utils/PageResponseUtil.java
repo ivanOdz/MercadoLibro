dcf4ad47cc72def20875dd72fe136d3c2c60facf
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-12 22:25:56 -0300

    FIX: adapted getResponse to queryparams and implemented getUriBuilderBooks and getUriBuilderExchanges

--- webapp/src/main/java/ar/edu/itba/paw/webapp/utils/PageResponseUtil.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/utils/PageResponseUtil.java
@@ -1,26 +1,60 @@
 package ar.edu.itba.paw.webapp.utils;
 
+import ar.edu.itba.paw.models.utils.ExchangeState;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.UriBuilder;
 
 public class PageResponseUtil {
 
-    public static Response getResponse(int page, int maxPage, UriInfo uriInfo, Response.ResponseBuilder builder) {
+    public static Response getResponse(int page, int maxPage, UriBuilder uri, Response.ResponseBuilder builder) {
 
-        builder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", 0).build(), "first");
-        builder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", maxPage).build(), "last");
+        builder.link(uri.clone().replaceQueryParam("page", 0).build(), "first");
+        builder.link(uri.clone().replaceQueryParam("page", maxPage).build(), "last");
 
         if (maxPage > 0) {
 
             if (page > 0 && page <= maxPage) {
-                builder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page - 1).build(), "prev");
+                builder.link(uri.clone().replaceQueryParam("page", page - 1).build(), "prev");
             }
 
             if (page >= 0 && page < maxPage) {
-                builder.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page + 1).build(), "next");
+                builder.link(uri.clone().replaceQueryParam("page", page + 1).build(), "next");
             }
         }
-
         return builder.build();
+    }
+
+    public static UriBuilder getUriBuilderBooks(UriBuilder uri, final long userId, final String search, final String sortType, final String state, final String genre) {
+        uri.queryParam("owner", userId);
+        if(search != null) {
+            uri.queryParam("search", search);
+        }
+        if(sortType != null) {
+            uri.queryParam("sort", sortType);
+        }
+        if(state != null) {
+            uri.queryParam("state", state);
+        }
+        if(genre != null) {
+            uri.queryParam("genre", genre);;
+        }
+        return uri;
+    }
+
+    public static UriBuilder getUriBuilderExchanges(UriBuilder uri, final long userId, final ExchangeState state, final Boolean isOfferer, final Boolean isRequester) {
+        uri.queryParam("user_id", userId);
+        if(state != null) {
+            uri.queryParam("state", state);
+        }
+        if(isOfferer != null) {
+            uri.queryParam("is_offerer", isOfferer);
+        }
+        if(isRequester != null) {
+            uri.queryParam("is_requester", isRequester);;
+        }
+        return uri;
     }
 }
