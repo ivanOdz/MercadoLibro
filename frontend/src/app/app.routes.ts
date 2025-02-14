3fc72d509fe634f2dd4c172f64d5a4a313825b23
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-13 23:41:11 -0300

    FIX: added routing to book model

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -16,6 +16,7 @@ import { BookHomeComponent } from './features/book-home/book-home.component';
 import {MyPublicationsComponent} from "./features/my-publications/my-publications.component";
 import {FavoritePublicationsComponent} from "./features/favorite-publications/favorite-publications.component";
 import {PublicationComponent} from "./features/publication-detail/publication.component";
+import {BookModelComponent} from "./features/book-model/book-model.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'publications', pathMatch: 'full'},
@@ -101,6 +102,12 @@ export const routes: Routes = [
     component: BookHomeComponent,
     title: 'Books',
     canActivate: [AuthGuard],
+  },
+  {
+    path: 'library',
+    component: BookModelComponent,
+    title: 'Library',
+    //canActivate: [AuthGuard],
   },
   //Error Pages
   {
