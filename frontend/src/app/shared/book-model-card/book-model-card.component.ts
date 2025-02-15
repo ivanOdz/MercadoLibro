bf01fd844fb7675fe651c982b585070336eec0ff
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-15 14:12:47 -0300

    FIX: added rating data to book model cards

--- frontend/src/app/shared/book-model-card/book-model-card.component.ts
+++ frontend/src/app/shared/book-model-card/book-model-card.component.ts
@@ -3,13 +3,24 @@ import {BookModel} from "../../core/models/bookModel.model";
 import {LowerCasePipe, NgForOf, NgIf} from "@angular/common";
 import {TranslatePipe} from "@ngx-translate/core";
 import {environment} from "../../../environments/environment";
+import {Button} from "primeng/button";
+import {Dialog} from "primeng/dialog";
+import {Divider} from "primeng/divider";
+import {Select} from "primeng/select";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-book-model-card',
   imports: [
     LowerCasePipe,
     TranslatePipe,
-    NgForOf
+    NgForOf,
+    Button,
+    Dialog,
+    Divider,
+    NgIf,
+    Select,
+    MatIcon
   ],
   templateUrl: './book-model-card.component.html',
   styleUrl: './book-model-card.component.css'
@@ -32,4 +43,25 @@ export class BookModelCardComponent {
   getBaseUrl() {
     return `${environment.production? environment.productionUrl  : environment.developmentUrl}`;
   }
+
+  getFormattedAuthors(authors: string[]): string {
+    const maxAuthors = 3;
+    if (authors.length > maxAuthors) {
+      return authors.slice(0, maxAuthors).join(', ') + ', ...';
+    }
+    return authors.join(', ');
+  }
+
+  getStarArray(averageRating: number): string[] {
+    const fullStars = Math.floor(averageRating);
+    const hasHalfStar = averageRating % 1 !== 0;
+    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+    return [
+      ...Array(fullStars).fill('star'),
+      ...(hasHalfStar ? ['star_half'] : []),
+      ...Array(emptyStars).fill('star_border')
+    ];
+  }
+
 }
