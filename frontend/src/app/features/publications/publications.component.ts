dcb4628e8823aa23691621c3f0ba6f46152a9ac0
tscheffer1 <tscheffer@itba.edu.ar>
2025-02-15 23:25:12 -0300

    CHORE: Publications page now working with card-page component (Deduplicating code)

--- frontend/src/app/features/publications/publications.component.ts
+++ frontend/src/app/features/publications/publications.component.ts
@@ -1,111 +1,41 @@
-import { Component, OnDestroy, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { PublicationService } from '../../core/services/publication.service';
-import { AuthService } from '../../core/services/auth.service';
-import { TranslatePipe } from '@ngx-translate/core';
-import { NavbarComponent } from "../../shared/navbar/navbar.component";
-import { FilterListComponent } from "../../shared/filter-list/filter-list.component";
-import { SortComponent } from "../../shared/sort/sort.component";
-import { distinctUntilChanged, filter, Subscription, switchMap, tap } from "rxjs";
-import { PublicationData } from "../../core/models/types";
-import { take } from "rxjs/operators";
-import { NgForOf, NgIf } from "@angular/common";
-import { ScrollPanelModule } from 'primeng/scrollpanel';
-import { PublicationCardComponent } from "../../shared/publication-card/publication.card";
-import { Pagination } from "../../core/models/pagination";
-import { PaginatorComponent } from "../../shared/paginator/paginator.component";
-import { InputGroup } from 'primeng/inputgroup';
-import { InputGroupAddon } from 'primeng/inputgroupaddon';
-import { InputText } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
+import { filter, Subscription } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { CardPageComponent } from '../../shared/card-page/card-page.component';
+import { PublicationCardComponent } from '../../shared/publication-card/publication.card';
+import { PublicationService } from "../../core/services/publication.service";
+import { AuthService } from "../../core/services/auth.service";
+import { ObservablePublicationData, PublicationData } from "../../core/models/types";
+import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-publications',
 	templateUrl: `./publications.component.html`,
 	styleUrls: ['./publications.component.css'],
 	standalone: true,
-	imports: [CommonModule, NavbarComponent, TranslatePipe, RouterModule, FilterListComponent, NgIf,
-		PublicationCardComponent, NgForOf, ScrollPanelModule, SortComponent, PaginatorComponent,
-		InputGroup, InputGroupAddon, InputText, ButtonModule, FormsModule]
+	imports: [CardPageComponent, PublicationCardComponent, TranslatePipe]
 
 })
-export class PublicationsComponent implements OnInit, OnDestroy {
+export class PublicationsComponent {
+	showConditionFilter: boolean = true;
+	showGenreFilter: boolean = true;
+	publications: PublicationData[] = [];
+	private subscription!: Subscription;
 
-	@ViewChild('searchInput') searchInput!: ElementRef;
-	
+	@ViewChild('publicationCard') publicationCard!: TemplateRef<any>;
+
 	constructor(
 		private publicationService: PublicationService,
 		private authService: AuthService,
-		private route: ActivatedRoute,
-		private router: Router,
-	) {}
-	
-	conditionHeaders: Record<string, string> = {};
-	genreHeaders: Record<string, string> = {};
-	isSearchActive = false;
-	lastSearchQuery: string | null = null;
-	
-	private subscription!: Subscription;
-	publications: PublicationData[] = [];
-	pagination: Pagination | null= null;
+		private route: ActivatedRoute
+	) { }
 
-	currentFilters = {
-		state: '',
-		genre: '',
-		page: 0,
-		search: ''
+	fetchPublications = (state: string, genre: string, search: string, page: number): ObservablePublicationData => {
+		return this.publicationService.getGeneralPublications(state, genre, page, search);
 	};
-
-	ngOnInit() {
-		this.fetchPublications();
-	}
 	
-	ngOnDestroy() {
-		this.subscription.unsubscribe();
-	}
-	
-	
-	search() {
-		if (this.currentFilters.search) {
-			this.isSearchActive = true;
-			this.router.navigate([], { queryParams: { search: this.currentFilters.search }, queryParamsHandling: 'merge' });
-		}
-		else {
-			this.router.navigate([], { queryParams: { search: null }, queryParamsHandling: 'merge' });
-			this.isSearchActive = false;
-		}
-		
-		this.lastSearchQuery = this.currentFilters.search;
-		this.searchInput.nativeElement.blur();
-	}
-
-	onSearchChange() {
-		// Dejo por si la necesito luego
-	}
-
-	onBlur() {
-		this.currentFilters.search = this.lastSearchQuery || '';
-	}
-	
-	removeFilter(filterKey: keyof typeof this.currentFilters) {
-		if (filterKey === 'state') {
-			this.currentFilters.state = '';
-		}	
-		else if (filterKey === 'genre') {
-			this.currentFilters.genre = '';
-		}
-  	
-		this.router.navigate([], {
-			relativeTo: this.route,
-			queryParams: { [filterKey]: null },
-			queryParamsHandling: 'merge',
-		});
-  	
-		this.fetchPublications();
-  	}
-  
+	/*
    fetchPublications(url: string | null = null) {
 		this.subscription = this.route.queryParams.pipe(
 			tap((params) => {
@@ -141,5 +71,5 @@ export class PublicationsComponent implements OnInit, OnDestroy {
 				console.error('Error:', err);
 			}
 		});
-	}
+	}*/
 }
