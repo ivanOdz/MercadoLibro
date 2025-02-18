ef260adcd754663cf8c1a5c1a86a1896d9beb980
tscheffer1 <tscheffer@itba.edu.ar>
2025-02-18 01:40:46 -0300

    CHORE: Error messages in login. (side note: authGuard is doing weird things)

--- frontend/src/app/features/auth/auth.component.ts
+++ frontend/src/app/features/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { Password } from 'primeng/password';
 import { InputText } from 'primeng/inputtext';
@@ -6,34 +7,60 @@ import { Checkbox } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import {TranslatePipe} from "@ngx-translate/core";
-import {LanguageService} from "../../core/services/language.service";
+import { TranslatePipe } from "@ngx-translate/core";
+import { LanguageService } from "../../core/services/language.service";
 
 @Component({
-  selector: 'app-auth',
-  standalone: true,
-  imports: [ButtonModule, InputText, Password, Checkbox, FormsModule, TranslatePipe],
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css',
+	selector: 'app-auth',
+	standalone: true,
+	imports: [CommonModule, ButtonModule, InputText, Password, Checkbox, FormsModule, TranslatePipe],
+	templateUrl: './auth.component.html',
+	styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-  username: string = '';
-  password: string = '';
-  rememberMe: boolean = false;
-
+	username: string = '';
+	password: string = '';
+	rememberMe: boolean = false;
+	errorMessage: string = '';
+  
   constructor(
-      private authService: AuthService,
-      private router: Router,
-      private route: ActivatedRoute,
-      private languageService: LanguageService // DO NOT DELETE! Translation would not work otherwise
-  ) {}
+	private authService: AuthService,
+	private router: Router,
+	private route: ActivatedRoute,
+	private languageService: LanguageService // DO NOT DELETE! Translation would not work otherwise
+	) { }
 
-  login() {
-    // Manda las credenciales para obtener al user
-    this.authService.login(this.username, this.password, this.rememberMe);
-
-    // Vuelve a la pagina en la que se produjo el 401.
-    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    this.router.navigateByUrl(returnUrl);
-  }
+	login() {
+	
+		this.errorMessage = '';
+		
+		this.authService.login(this.username, this.password, this.rememberMe).subscribe({
+			next: () => {
+				const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+				this.router.navigateByUrl(returnUrl);
+			},
+			error: (err) => {
+				if (err.status === 401) {
+					this.errorMessage = 'INCORRECT_CREDENTIALS';
+				} else if (err.status === 403) {
+					this.errorMessage = 'NO_PERMISSION';
+				} else if (err.status === 500) {
+					this.errorMessage = 'SERVER_ERROR';
+				} else {
+					this.errorMessage = 'UNEXPECTED_ERROR';
+				}
+			},
+			complete: () => {
+				if (!this.authService.isAuthenticated$) {
+					this.errorMessage = 'INCORRECT_CREDENTIALS';
+				}
+			}
+		});
+		
+		if (this.authService.isAuthenticated$) {
+			const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+			console.log(returnUrl);
+			this.router.navigateByUrl(returnUrl);
+		}
+	}
 }
