88ca6f081540c564103ca0c39adb0488a140cfe0
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-18 16:52:52 -0300

    CHORE: errors images

--- frontend/src/app/features/errors/not-found/not-found.component.ts
+++ frontend/src/app/features/errors/not-found/not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ErrorComponent } from '../error/error.component';
+import {TranslatePipe} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-not-found',
-  imports: [
-    ErrorComponent
-  ],
+    imports: [
+        ErrorComponent,
+        TranslatePipe
+    ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
