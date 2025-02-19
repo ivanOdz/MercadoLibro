006c556b990b929c06617d6ebb27152d19794a76
Maggie Taurian <mtaurian@itba.edu.ar>
2025-02-18 22:14:39 -0300

    FIX: added 400

--- frontend/src/app/features/errors.routes.ts
+++ frontend/src/app/features/errors.routes.ts
@@ -3,8 +3,10 @@ import {AuthGuard} from "../core/guards/auth.guard";
 import {NotFoundComponent} from "./errors/not-found/not-found.component";
 import {UnauthorizedComponent} from "./errors/unauthorized/unauthorized.component";
 import {ForbiddenComponent} from "./errors/forbidden/forbidden.component";
+import {BadRequestComponent} from "./errors/bad-request/bad-request.component";
 
 export const errorRoutes: Routes = [
+    { path: '400', component: BadRequestComponent, title: 'Bad Request' },
     { path: '401', component: UnauthorizedComponent, title: 'Unauthorized' },
     { path: '403', component: ForbiddenComponent, title: 'Forbidden' },
 ];
