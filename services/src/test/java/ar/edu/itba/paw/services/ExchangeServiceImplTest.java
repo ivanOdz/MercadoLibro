3d6d347d7c027acf50b167a16dca7fe29d9b63a4
tscheffer1 <tscheffer@itba.edu.ar>
2025-02-19 00:39:30 -0300

    CHORE: ExchangeService & BookService Tests completed

--- services/src/test/java/ar/edu/itba/paw/services/ExchangeServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/ExchangeServiceImplTest.java
@@ -6,13 +6,18 @@ import ar.edu.itba.paw.interfaces.services.*;
 import ar.edu.itba.paw.models.*;
 import ar.edu.itba.paw.models.utils.PublicationState;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.sql.Timestamp;
 import java.util.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -42,16 +47,16 @@ public class ExchangeServiceImplTest {
     private static final long USER_ID_2 = 2L;
     private static final long BOOK_ID_2 = 2L;
     private static final long PUBLICATION_ID_1 = 1L;
-    private static final long PUBLICATION_ID_2 = 1L;
+    private static final long PUBLICATION_ID_2 = 2L;
+    private static final long OFFERER_ID_1 = 1L;
+    private static final long REQUESTER_ID_1 = 2L;
     private static final PublicationState PUBLICATION_STATE_1 = PublicationState.OFFERED;
     private static final PublicationState PUBLICATION_STATE_2 = PublicationState.CURRENT;
     
     private static final long LOCATION_ID = 1L;  
     private static final int ACCEPT_CODE = 123456789;
 
-	/*
-    @Test(expected = ExchangeBadRequestException.class)
-	public void testExchangeBadRequestException() {
+	public void testInitializeExchange() {
 		
         Location location = mock(Location.class);
         List<Location> locations = new ArrayList<>();
@@ -65,6 +70,7 @@ public class ExchangeServiceImplTest {
         Publication publication_1 = mock(Publication.class);
         Publication publication_2 = mock(Publication.class);
         Exchange exchange = mock(Exchange.class);
+        Exchange returnExchange = null;
         
         user_1.setUserId(USER_ID_1);
         book_1.setBookId(BOOK_ID_1);
@@ -85,26 +91,28 @@ public class ExchangeServiceImplTest {
         exchange.setOfferer(publication_1);
         exchange.setRequester(publication_2);
         
-        exchangeServiceImpl.exchange((int)exchange.getAcceptCode(), true);
+        when(bookService.getBookById(BOOK_ID_1)).thenReturn(book_1);
+        when(publicationService.createPublication(BOOK_ID_1, USER_ID_1, LOCATION_ID)).thenReturn(publication_1);
+        when(exchangeDao.createExchange(BOOK_ID_1, REQUESTER_ID_1, ACCEPT_CODE, any())).thenReturn(exchange);
+        
+        returnExchange = exchangeServiceImpl.initializeExchange(BOOK_ID_1, LOCATION_ID, OFFERER_ID_1);
+        
+        Assert.assertNotNull(returnExchange);
+        Assert.assertEquals(EXCHANGE_ID_1, returnExchange.getExchangeId().longValue());
+        Assert.assertEquals(PUBLICATION_ID_1, returnExchange.getOfferer().getPublicationId().longValue());
+        Assert.assertEquals(PUBLICATION_ID_2, returnExchange.getRequester().getPublicationId().longValue());
+        Assert.assertEquals(ACCEPT_CODE, returnExchange.getAcceptCode());
 	}
-	 */
 
-	
 	@Test(expected = ExchangeBadRequestException.class)
 	public void testGetExchangeByIdNotFoundException() {
 		
 		exchangeServiceImpl.getExchangeById(EXCHANGE_ID_1);
 	}
 	
-	/*@Test(expected = ExchangeBadRequestException.class)
-	public void testcofirmRequesterBadRequestException() {
-		
-		//exchangeServiceImpl.cofirmRequester(ACCEPT_CODE);
-	}
-	
 	@Test(expected = ExchangeBadRequestException.class)
-	public void testcofirmOffererBadRequestException() {
+	public void testUpdateExchangeBadRequestException() {
 		
-		//exchangeServiceImpl.cofirmOfferer(ACCEPT_CODE);
-	}*/
+		exchangeServiceImpl.updateExchange(ACCEPT_CODE, true, true);
+	}
 }
